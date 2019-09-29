ed72c44a4858cc40fc734337e8bc4285d09a1bb8
ColeTrammer <cole.trammer@gmail.com>
2019-09-28 17:24:39 -0700

    simple utility to test writing to files

---
+++ initrd/programs/fwrite.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("Usage: %s <file-name> <text>\n", argv[0]);
+        return 0;
+    }
+
+    FILE *f = fopen(argv[1], "w");
+    if (!f) {
+        perror("fwrite");
+        return 1;
+    }
+
+    size_t len = strlen(argv[2]) + 1;
+    if (fwrite(argv[2], sizeof(char), len, f) != len) {
+        perror("fwrite");
+        return 1;
+    }
+
+    if (fclose(f) == EOF) {
+        perror("fwrite");
+        return 1;
+    }
+
+    return 0;
+}
