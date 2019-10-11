9e6e6099999693754ca925028ce7422852769f7f
ColeTrammer <cole.trammer@gmail.com>
2019-10-11 14:46:20 -0700

    enable FPU but don't use it

--- kernel/include/kernel/irqs/handlers.h
+++ kernel/include/kernel/irqs/handlers.h
@@ -5,6 +5,9 @@
 
 void init_irq_handlers();
 
+void handle_invalid_opcode_entry();
+void handle_fpu_exception_entry();
+void handle_device_not_available_entry();
 void handle_double_fault_entry();
 void handle_general_protection_fault_entry();
 void handle_page_fault_entry();
@@ -14,5 +17,8 @@ void sys_call_entry();
 void handle_double_fault();
 void handle_general_protection_fault(uintptr_t error);
 void handle_page_fault(uintptr_t address, uintptr_t error);
+void handle_invalid_opcode();
+void handle_fpu_exception();
+void handle_device_not_available();
 
 #endif /* _KERNEL_INTERRUPTS_INTERRUPTS_H */
--- kernel/irqs/handlers.c
+++ kernel/irqs/handlers.c
@@ -8,9 +8,12 @@
 #include <kernel/hal/output.h>
 
 void init_irq_handlers() {
+    register_irq_handler(&handle_invalid_opcode_entry, 6, false);
+    register_irq_handler(&handle_device_not_available_entry, 7, false);
     register_irq_handler(&handle_double_fault_entry, 8, false);
     register_irq_handler(&handle_general_protection_fault_entry, 13, false);
     register_irq_handler(&handle_page_fault_entry, 14, false);
+    register_irq_handler(&handle_fpu_exception_entry, 16, false);
 
     register_irq_handler(&sys_call_entry, 128, true);
 
@@ -33,5 +36,23 @@ void handle_page_fault(uintptr_t address, uintptr_t error) {
     dump_registers_to_screen();
     printf("\n\033[31m%s: Error %lX\n", "Page Fault", error);
     printf("Address: %#.16lX\033[0m\n", address);
+    abort();
+}
+
+void handle_invalid_opcode() {
+    dump_registers_to_screen();
+    printf("\n\033[31m%s\033[0m\n", "Invalid Opcode");
+    abort();
+}
+
+void handle_fpu_exception() {
+    dump_registers_to_screen();
+    printf("\n\033[31m%s\033[0m\n", "FPU Exception");
+    abort();
+}
+
+void handle_device_not_available() {
+    dump_registers_to_screen();
+    printf("\n\033[31m%s\033[0m\n", "Device Not Available Exception");
     abort();
 }
