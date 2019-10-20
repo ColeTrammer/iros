27a4af4430559c289e242725565b9335fff1be9c
ColeTrammer <cole.trammer@gmail.com>
2019-10-19 22:31:19 -0700

    working setenv and unsetenv

--- libc/stdlib/env.c
+++ libc/stdlib/env.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <errno.h>
+#include <stdio.h>
 
 #define ENV_NUM_BITS_BASE (10 * 64)
 #define ENV_BUF_INC 20
@@ -34,7 +35,16 @@ static void set_allocated(size_t env_index) {
     bitmap[uint_off] |= (1ULL << bit_off);
 }
 
-static int env_add(char *s) {
+static void unset_allocated(size_t env_index) {
+    size_t bit_off = env_index % 64;
+    size_t uint_off = env_index / 64;
+
+    assert(env_index < num_bits);
+
+    bitmap[uint_off] &= ~(1ULL << bit_off);
+}
+
+static int env_add(char *s, bool should_set_allocated) {
     if (!is_environ_malloced) {
         max_env_vars = num_env_vars + ENV_BUF_INC;
         // Calloc ensures everything will be null terminated
@@ -56,8 +66,31 @@ static int env_add(char *s) {
         }
     }
 
+    if (should_set_allocated) {
+        set_allocated(num_env_vars);
+    }
+
     environ[num_env_vars++] = s;
     environ[num_env_vars] = NULL;
+    return 0;
+}
+
+static int env_del(size_t index) {
+    if (is_allocated(index)) {
+        free(environ[index]);
+    }
+
+    for (; index < num_env_vars; index++) {
+        if (is_allocated(index + 1)) {
+            set_allocated(index);
+        } else {
+            unset_allocated(index);
+        }
+
+        environ[index] = environ[index + 1];
+    }
+
+    num_env_vars--;
     return 0;
 }
 
@@ -88,29 +121,84 @@ int putenv(char *string) {
     }
 
     char **e = environ;
+    size_t i = 0;
     while (*e != NULL) {
         // Compare up until the `=` character
         if (strncmp(*e, string, equals - string + 1) == 0) {
+            if (is_allocated(i)) {
+                free(*e);
+            }
             *e = string;
             return 0;
         }
         e++;
+        i++;
     }
 
     // At this point we know we have to add it to the array
-    return env_add(string);
+    return env_add(string, false);
 }
 
 int setenv(const char *__restrict name, const char *__restrict value, int overwrite) {
-    (void) name;
-    (void) value;
-    (void) overwrite;
-    return -1;
+    if (name == NULL || strchr(name, '=') != NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    size_t nl = strlen(name);
+    char *to_add = malloc(nl + strlen(value) + 2);
+    if (to_add == NULL) {
+        errno = ENOMEM;
+        return -1;
+    }
+
+    strcpy(to_add, name);
+    strcat(to_add, "=");
+    strcat(to_add, value);
+
+    char **e = environ;
+    size_t i = 0;
+    while (*e != NULL) {
+        if (strncmp(*e, to_add, nl + 1) == 0) {
+            if (!overwrite) {
+                return 0;
+            }
+
+            if (is_allocated(i)) {
+                free(*e);
+            }
+
+            *e = to_add;
+            set_allocated(i);
+            return 0;
+        }
+        e++;
+        i++;
+    }
+
+    return env_add(to_add, true);
 }
 
 int unsetenv(const char *name) {
-    (void) name;
-    return -1;
+    if (name == NULL || strchr(name, '=') != NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    size_t nl = strlen(name);
+
+    char **e = environ;
+    size_t i = 0;
+    while (*e != NULL) {
+        if (memcmp(*e, name, nl) == 0 && (*e)[nl] == '=') {
+            return env_del(i);
+        }
+        e++;
+        i++;
+    }
+
+    // Do nothing
+    return 0;
 }
 
 void init_env() {
