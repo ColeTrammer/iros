7e9e72b2ece2c5168a725d7d5a393cd2dcfc1e52
ColeTrammer <cole.trammer@gmail.com>
2019-10-20 22:21:19 -0700

    added echo utility

---
+++ initrd/programs/strtoken_test.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+
+int main () {
+   char str[80] = "echo asdf  asdf\n";
+   const char *s = " \r\t\n\a";
+   char *token;
+   
+   /* get the first token */
+   token = strtok(str, s);
+   
+   /* walk through other tokens */
+   while( token != NULL ) {
+      printf( " %s\n", token );
+    
+      token = strtok(NULL, s);
+   }
+   
+   return(0);
+}
---
+++ userland/echo/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    for (size_t i = 1; i < argc; i++) {
+        printf("%s", argv[i]);
+        if (i < argc - 1) {
+            printf("%c", ' ');
+        }
+    }
+
+    printf("%c", '\n');
+    return 0;
+}
