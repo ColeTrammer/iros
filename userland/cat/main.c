22401da0981c4f1e2a052eacd19c35def1fd56fe
ColeTrammer <cole.trammer@gmail.com>
2019-10-20 19:03:20 -0700

    make cat use getline instead of fseek/ftell so that it can read from pipes properly

--- userland/cat/main.c
+++ userland/cat/main.c
@@ -13,42 +13,20 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    if (argc > 2) {
-        printf("Usage: %s [file-name]\n", argv[0]);
-        return 0;
-    }
+    char *line = NULL;
+    size_t line_max = 0;
 
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
-        perror("cat");
-        return 1;
-    }
+    for (size_t i = 1; i < argc; i++) {
+        FILE *f = fopen(argv[i], "r");
+        if (f == NULL) {
+            perror("cat");
+            return 1;
+        }
 
-    if (fseek(file, 0, SEEK_END) < 0) {
-        perror("cat");
-        return 1;
+        while (getline(&line, &line_max, f) != -1) {
+            printf("%s", line);
+        }
     }
-
-    off_t size = ftell(file);
-    if (size < 0) {
-        perror("cat");
-        return 1;
-    }    
-
-    if (fseek(file, 0, SEEK_SET) < 0) {
-        perror("cat");
-        return 1;
-    }
-
-    char *buffer = malloc(size + 1);
-    int read = fread(buffer, 1, size, file);
-    if (read == 0) {
-        perror("cat");
-        return 1;
-    }
-    buffer[size] = '\0';
-    printf("%s", buffer);
-    fclose(file);
 
     return 0;
 }
