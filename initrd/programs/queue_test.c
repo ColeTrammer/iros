8d5ba60f7a9f36d2575b1afff09f7c8081f83045
ColeTrammer <cole.trammer@gmail.com>
2019-10-26 14:15:12 -0700

    libc: add insque and remque functions

---
+++ initrd/programs/queue_test.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <search.h>
+
+struct element {
+    struct element *forward;
+    struct element *backward;
+    char *name;
+};
+
+static struct element *
+new_element(void)
+{
+    struct element *e;
+
+    e = malloc(sizeof(struct element));
+    if (e == NULL) {
+        fprintf(stderr, "malloc() failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return e;
+}
+
+int
+main(int argc, char *argv[])
+{
+    struct element *first, *elem, *prev;
+    int circular, opt, errfnd;
+
+    /* The "-c" command-line option can be used to specify that the
+        list is circular */
+
+    errfnd = 0;
+    circular = 0;
+    while ((opt = getopt(argc, argv, "c")) != -1) {
+        switch (opt) {
+        case 'c':
+            circular = 1;
+            break;
+        default:
+            errfnd = 1;
+            break;
+        }
+    }
+
+    if (errfnd || optind >= argc) {
+        fprintf(stderr,  "Usage: %s [-c] string...\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Create first element and place it in the linked list */
+
+    elem = new_element();
+    first = elem;
+
+    elem->name = argv[optind];
+
+    if (circular) {
+        elem->forward = elem;
+        elem->backward = elem;
+        insque(elem, elem);
+    } else {
+        insque(elem, NULL);
+    }
+
+    /* Add remaining command-line arguments as list elements */
+
+    while (++optind < argc) {
+        prev = elem;
+
+        elem = new_element();
+        elem->name = argv[optind];
+
+        insque(elem, prev);
+        if (optind % 2 == 0) {
+            printf("Removing: %s\n", elem->name);
+            remque(elem);
+            elem = prev;
+        }
+    }
+
+    /* Traverse the list from the start, printing element names */
+
+    printf("Traversing completed list:\n");
+    elem = first;
+    do {
+        printf("    %s\n", elem->name);
+        elem = elem->forward;
+    } while (elem != NULL && elem != first);
+
+    if (elem == first)
+        printf("That was a circular list\n");
+
+    exit(EXIT_SUCCESS);
+}
---
+++ libc/include/search.h
@@ -0,0 +1,42 @@
+#ifndef _SEARCH_H
+#define _SEARCH_H 1
+
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+typedef struct entry {
+    char *key;
+    void *data;
+} ENTRY;
+
+typedef enum {
+    FIND, ENTER
+} ACTION;
+
+typedef enum {
+    preorder, postorder, endorder, leaf
+} VISIT;
+
+int hcreate(size_t size);
+void hdestroy(void);
+ENTRY *hsearch(ENTRY entry, ACTION action);
+
+void *lfind(const void *key, const void *base, size_t *nmemb, size_t size, int(*compar)(const void *a, const void *b));
+void *lsearch(const void *key, const void *base, size_t *nmemb, size_t size, int(*compar)(const void *a, const void *b));
+
+void insque(void *elem, void *prev);
+void remque(void *elem);
+
+void *tfind(const void *key, void **rootp, int (*compar)(const void *a, const void *b));
+void *tsearch(const void *key, void **rootp, int (*compar)(const void *a, const void *b));
+void *tdelete(const void *__restrict key, void **__restrict rootp, int (*compar)(const void *a ,const void *b));
+void twalk(const void *root, void (*action)(const void *nodep, const VISIT which, const int depth));
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _SEARCH_H */
