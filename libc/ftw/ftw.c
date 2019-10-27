86547382771a32346448690acb4620f1cbbf0918
ColeTrammer <cole.trammer@gmail.com>
2019-10-26 21:36:00 -0700

    libc: nftw supports depth first search too

--- libc/ftw/ftw.c
+++ libc/ftw/ftw.c
@@ -16,6 +16,9 @@ struct ftw_dirent {
     struct ftw_dirent *next;
     struct ftw_dirent *prev;
     char *path;
+    bool expanded;
+    struct stat stat_struct;
+    int depth;
 };
 
 static struct ftw_dirent *start = NULL;
@@ -27,7 +30,7 @@ static void free_dirent(struct ftw_dirent *d) {
     free(d);
 }
 
-static int add_dirent(char *path) {
+static int add_dirent(char *path, int depth) {
     assert(path);
 
     struct ftw_dirent *d = malloc(sizeof(struct ftw_dirent));
@@ -36,6 +39,8 @@ static int add_dirent(char *path) {
     }
 
     d->path = path;
+    d->expanded = false;
+    d->depth = depth;
     if (!end || !start) {
         start = end = d;
         insque(d, NULL);
@@ -63,6 +68,26 @@ static struct ftw_dirent *consume_dirent() {
     return d;
 }
 
+static struct ftw_dirent *pop_dirent() {
+    assert(size > 0);
+    assert(end);
+
+    struct ftw_dirent *d = end;
+    end = end->prev;
+    if (!end) {
+        start = NULL;
+    }
+    remque(d);
+    size--;
+
+    return d;
+}
+
+static struct ftw_dirent *peek_dirent() {
+    assert(end);
+    return end;
+}
+
 static int ftw_call(fn_t fn, const char *path, struct stat *stat_struct, int depth) {
     int type = 0;
     if (!stat_struct) {
@@ -83,7 +108,7 @@ static int ftw_call(fn_t fn, const char *path, struct stat *stat_struct, int dep
     return fn(path, stat_struct, type, &ftw_buf);
 }
 
-static int ftw_build_dirents(const char *path) {
+static int ftw_build_dirents(const char *path, int depth) {
     DIR *d = opendir(path);
     if (!d) {
         return -1;
@@ -99,13 +124,13 @@ static int ftw_build_dirents(const char *path) {
         strcpy(new_path, path);
         strcat(new_path, "/");
         strcat(new_path, ent->d_name);
-        add_dirent(new_path);
+        add_dirent(new_path, depth);
     }
 
     return closedir(d);
 }
 
-static int ftw_traverse(fn_t fn) {
+static int ftw_traverse_bf(fn_t fn) {
     int depth = 0;
     while (size > 0) {
         size_t size_save = size;
@@ -114,7 +139,7 @@ static int ftw_traverse(fn_t fn) {
 
             struct stat stat_struct;
             if (!stat(d->path, &stat_struct) && S_ISDIR(stat_struct.st_mode)) {
-                ftw_build_dirents(d->path);
+                ftw_build_dirents(d->path, depth + 1);
             }
 
             int ret = ftw_call(fn, d->path, &stat_struct, depth);
@@ -132,22 +157,50 @@ static int ftw_traverse(fn_t fn) {
     return 0;
 }
 
+static int ftw_traverse_df(fn_t fn) {
+    while (size > 0) {
+        struct ftw_dirent *d = peek_dirent();
+
+        if (!d->expanded) {
+            if (!stat(d->path, &d->stat_struct) && S_ISDIR(d->stat_struct.st_mode)) {
+                ftw_build_dirents(d->path, d->depth + 1);
+                d->expanded = true;
+                continue;
+            }
+        }
+
+        pop_dirent();
+        int ret = ftw_call(fn, d->path, &d->stat_struct, d->depth);
+
+        free_dirent(d);
+
+        if (ret) {
+            return ret;
+        }
+    }
+
+    return 0;
+}
+
 int nftw(const char *path, fn_t fn, int fd_limit, int flags) {
     if (fd_limit <= 0) {
         return -1;
     }
 
     assert(!(flags & FTW_CHDIR));
-    assert(!(flags & FTW_DEPTH));
     assert(!(flags & FTW_PHYS));
     assert(strrchr(path, '/') != path + strlen(path) - 1); // Shoud be fixed
 
-    int ret = add_dirent(strdup(path));
+    int ret = add_dirent(strdup(path), 0);
     if (ret != 0) {
         goto cleanup;
     }
 
-    ret = ftw_traverse(fn);
+    if (flags & FTW_DEPTH) {
+        ret = ftw_traverse_df(fn);
+    } else {
+        ret = ftw_traverse_bf(fn);
+    }
 
 cleanup:
     while (size > 0) {
