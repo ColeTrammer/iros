e9dba8f33115abe7cf7e78c367c0e853fad2b348
ColeTrammer <cole.trammer@gmail.com>
2019-11-02 23:27:42 -0700

    libc: execl and ENOEXEC handling
    Now, if you try to execvp something that is not
    executable, it will run in the shell.

--- libc/unistd/exec.c
+++ libc/unistd/exec.c
@@ -1,8 +1,98 @@
 #include <assert.h>
 #include <errno.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#define EXEC_BUF_INC 100
+
+int execl(const char *path, const char *arg, ...) {
+    assert(arg);
+
+    va_list args;
+    va_start(args, arg);
+
+    char **arg_list = malloc(EXEC_BUF_INC * sizeof(char*));
+    size_t i = 1;
+    size_t max = 0;
+
+    char *a;
+    while ((a = va_arg(args, char*))) {
+        if (i + 1 >= max) {
+            max += EXEC_BUF_INC;
+            arg_list = realloc(arg_list, max * sizeof(char*));
+        }
+
+        arg_list[i++] = a;
+    }
+
+    arg_list[0] = (char*) arg;
+    arg_list[i] = NULL;
+
+    int ret = execve(path, arg_list, environ);
+
+    va_end(args);
+    return ret;
+}
+
+int execle(const char *path, const char *arg, ...) {
+    assert(arg);
+
+    va_list args;
+    va_start(args, arg);
+
+    char **arg_list = malloc(EXEC_BUF_INC * sizeof(char*));
+    size_t i = 1;
+    size_t max = 0;
+
+    char *a;
+    while ((a = va_arg(args, char*))) {
+        if (i + 1 >= max) {
+            max += EXEC_BUF_INC;
+            arg_list = realloc(arg_list, max * sizeof(char*));
+        }
+
+        arg_list[i++] = a;
+    }
+
+    arg_list[0] = (char*) arg;
+    arg_list[i] = NULL;
+
+    int ret = execve(path, arg_list, va_arg(args, char**));
+
+    va_end(args);
+    return ret;
+}
+
+int execlp(const char *name, const char *arg, ...) {
+    assert(arg);
+
+    va_list args;
+    va_start(args, arg);
+
+    char **arg_list = malloc(EXEC_BUF_INC * sizeof(char*));
+    size_t i = 1;
+    size_t max = 0;
+
+    char *a;
+    while ((a = va_arg(args, char*))) {
+        if (i + 1 >= max) {
+            max += EXEC_BUF_INC;
+            arg_list = realloc(arg_list, max * sizeof(char*));
+        }
+
+        arg_list[i++] = a;
+    }
+
+    arg_list[0] = (char*) arg;
+    arg_list[i] = NULL;
+
+    int ret = execvp(name, arg_list);
+
+    va_end(args);
+    return ret;
+}
 
 int execv(const char *path, char *const argv[]) {
     return execve(path, argv, environ);
@@ -23,6 +113,7 @@ int execvpe(const char *file, char *const argv[], char *const envp[]) {
         pathvar = "/bin:/usr/bin";
     }
 
+    bool e_access = false;
     int error = 0;
     char *path = strdup(pathvar);
 
@@ -35,10 +126,19 @@ int execvpe(const char *file, char *const argv[], char *const envp[]) {
 
         int ret = execve(test_file, argv, envp);
         assert(ret == -1);
-        if (errno == ENOENT) {
-            if (error == 0) { 
-                error = ENOENT;
-            }
+        if (errno == EACCES) {
+            e_access = true;
+        } else if (errno == ENOEXEC) {
+            size_t num_args = 0;
+            while (argv[num_args++] != NULL);
+
+            char **new_args = malloc((num_args + 1) + sizeof(char*));
+            new_args[0] = "/bin/sh";
+            new_args[1] = test_file;
+            memcpy(&new_args[2], &argv[1], (num_args - 1) * sizeof(char*));
+            execve(new_args[0], new_args, envp);
+            free(new_args);
+            return -1;
         } else {
             error = errno;
         }
@@ -49,6 +149,6 @@ int execvpe(const char *file, char *const argv[], char *const envp[]) {
     }
 
     free(path);
-    errno = error;
+    errno = e_access ? EACCES : error;
     return -1;
 }
