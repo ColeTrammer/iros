84325455e4e36f7dd25282eb298d18e3f8c104db
ColeTrammer <cole.trammer@gmail.com>
2019-11-13 15:01:11 -0800

    libc: stdbool won't try to define bool/true/false if __cplusplus

--- libc/include/stdbool.h
+++ libc/include/stdbool.h
@@ -1,10 +1,14 @@
 #ifndef _STDBOOL_H
 #define _STDBOOL_H 1
 
+#ifndef __cplusplus
+
 #define bool _Bool
 
 #define false 0
 #define true 1
+
+#endif /* __cplusplus */
 
 #define __bool_true_false_are_defined 1
 
