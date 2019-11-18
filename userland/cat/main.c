899110a10f96461b8e61cdeb51c449ad2f2fa56c
ColeTrammer <cole.trammer@gmail.com>
2019-11-17 21:18:01 -0800

    cat: use intelligent buffering to write faster

--- userland/cat/main.c
+++ userland/cat/main.c
@@ -1,31 +1,32 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-    if (argc == 1) {
-        int c;
-        while ((c = getchar()) != EOF) {
-            printf("%c", c);
-        }
-
-        return 0;
-    }
-
-    char *line = NULL;
-    size_t line_max = 0;
-
-    for (int i = 1; i < argc; i++) {
-        FILE *f = fopen(argv[i], "r");
-        if (f == NULL) {
+    int fd = STDIN_FILENO;
+    if (argc == 2) {
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1) {
             perror("cat");
             return 1;
         }
+    }
 
-        while (getline(&line, &line_max, f) != -1) {
-            printf("%s", line);
+    char buf[BUFSIZ];
+    ssize_t amount;
+    while ((amount = read(fd, buf, BUFSIZ)) > 0) {
+        if (write(STDOUT_FILENO, buf, amount) != amount) {
+            perror("cat");
+            return 1;
         }
+    }
+
+    if (amount != 0) {
+        perror("cat");
+        return 1;
     }
 
     return 0;
