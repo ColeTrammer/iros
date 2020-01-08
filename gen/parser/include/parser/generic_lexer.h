059ae3686e87475aac43f77f9863126860de053b
ColeTrammer <cole.trammer@gmail.com>
2020-01-07 20:14:56 -0800

    parser: add missing generic_lexer.h

---
+++ gen/parser/include/parser/generic_lexer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <parser/generic_token.h>
+
+template<typename TokenType, typename Value> class GenericLexer {
+public:
+    GenericLexer() {}
+    virtual ~GenericLexer() {}
+
+    virtual TokenType peek_next_token_type() const = 0;
+    virtual const Value& peek_next_token_value() const = 0;
+    virtual void advance() = 0;
+};
