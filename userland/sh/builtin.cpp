968c4de97960c79e94f7fb14409929262363f44f
ColeTrammer <cole.trammer@gmail.com>
2020-01-07 19:43:51 -0800

    sh: test/[ builtin

--- userland/sh/builtin.cpp
+++ userland/sh/builtin.cpp
@@ -1,9 +1,11 @@
 #include <assert.h>
+#include <fcntl.h>
 #include <liim/hash_map.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #include "builtin.h"
@@ -414,6 +416,179 @@ static int op_set(char **argv) {
     return 0;
 }
 
+static int op_test(char **argv) {
+    int argc = 0;
+    while (argv[++argc]) {
+    }
+
+    if (strcmp(argv[0], "[") == 0) {
+        if (strcmp(argv[argc - 1], "]") != 0) {
+            fprintf(stderr, "%s: expected `]'\n", argv[0]);
+            return 1;
+        }
+
+        argc--;
+    }
+
+    argc--;
+    argv++;
+
+    bool invert = false;
+    if (strcmp(argv[0], "!") == 0) {
+        invert = true;
+        argc--;
+        argv++;
+    }
+
+    if (argc == 1) {
+        return strlen(argv[0]) != 0 ? invert : !invert;
+    }
+
+    if (argc == 2) {
+        if (strcmp(argv[0], "-b") == 0) {
+            struct stat st;
+            if (stat(argv[1], &st)) {
+                return 1;
+            }
+
+            return S_ISBLK(st.st_mode) ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-c") == 0) {
+            struct stat st;
+            if (stat(argv[1], &st)) {
+                return 1;
+            }
+
+            return S_ISCHR(st.st_mode) ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-d") == 0) {
+            struct stat st;
+            if (stat(argv[1], &st)) {
+                return 1;
+            }
+
+            return S_ISDIR(st.st_mode) ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-e") == 0) {
+            struct stat st;
+            if (stat(argv[1], &st)) {
+                return 1;
+            }
+
+            return invert;
+        }
+        if (strcmp(argv[0], "-f") == 0) {
+            struct stat st;
+            if (stat(argv[1], &st)) {
+                return 1;
+            }
+
+            return S_ISREG(st.st_mode) ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-g") == 0) {
+            struct stat st;
+            if (stat(argv[1], &st)) {
+                return 1;
+            }
+
+            return (st.st_mode & 02000) ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-h") == 0 || strcmp(argv[0], "-L") == 0) {
+            struct stat st;
+            if (lstat(argv[1], &st)) {
+                return 1;
+            }
+
+            return S_ISLNK(st.st_mode) ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-n") == 0) {
+            return strlen(argv[1]) != 0 ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-p") == 0) {
+            struct stat st;
+            if (stat(argv[1], &st)) {
+                return 1;
+            }
+
+            return S_ISFIFO(st.st_mode) ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-r") == 0) {
+            if (access(argv[1], R_OK)) {
+                return !invert;
+            }
+            return invert;
+        }
+        if (strcmp(argv[0], "-S") == 0) {
+            struct stat st;
+            if (stat(argv[1], &st)) {
+                return 1;
+            }
+
+            return S_ISOCK(st.st_mode) ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-s") == 0) {
+            struct stat st;
+            if (stat(argv[1], &st)) {
+                return 1;
+            }
+
+            return st.st_size ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-t") == 0) {
+            return isatty(atoi(argv[1])) ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-u") == 0) {
+            struct stat st;
+            if (stat(argv[1], &st)) {
+                return 1;
+            }
+
+            return (st.st_mode & 04000) ? invert : !invert;
+        }
+        if (strcmp(argv[0], "-w") == 0) {
+            if (access(argv[1], W_OK)) {
+                return !invert;
+            }
+            return invert;
+        }
+        if (strcmp(argv[0], "-x") == 0) {
+            if (access(argv[1], X_OK)) {
+                return !invert;
+            }
+            return invert;
+        }
+        if (strcmp(argv[0], "-z") == 0) {
+            return strlen(argv[1]) == 0 ? invert : !invert;
+        }
+    }
+
+    if (argc == 3) {
+        if (strcmp(argv[1], "-lt") == 0) {
+            return atol(argv[0]) < atol(argv[2]) ? invert : !invert;
+        }
+        if (strcmp(argv[1], "-eq") == 0) {
+            return atol(argv[0]) == atol(argv[2]) ? invert : !invert;
+        }
+        if (strcmp(argv[1], "-gt") == 0) {
+            return atol(argv[0]) > atol(argv[2]) ? invert : !invert;
+        }
+        if (strcmp(argv[1], "-ge") == 0) {
+            return atol(argv[0]) >= atol(argv[2]) ? invert : !invert;
+        }
+        if (strcmp(argv[1], "-le") == 0) {
+            return atol(argv[0]) <= atol(argv[2]) ? invert : !invert;
+        }
+        if (strcmp(argv[1], "==") == 0) {
+            return strcmp(argv[0], argv[2]) == 0 ? invert : !invert;
+        }
+        if (strcmp(argv[1], "!=") == 0) {
+            return strcmp(argv[0], argv[2]) != 0 ? invert : !invert;
+        }
+    }
+
+    return !invert;
+}
+
 static struct builtin_op builtin_ops[NUM_BUILTINS] = {
     { "exit", op_exit, true },       { "cd", op_cd, true },         { "echo", op_echo, false },
     { "export", op_export, true },   { "unset", op_unset, true },   { "jobs", op_jobs, true },
@@ -422,7 +597,8 @@ static struct builtin_op builtin_ops[NUM_BUILTINS] = {
     { ":", op_colon, true },         { "break", op_break, true },   { "continue", op_continue, true },
     { ".", op_dot, true },           { "source", op_dot, true },    { "alias", op_alias, true },
     { "unalias", op_unalias, true }, { "return", op_return, true }, { "shift", op_shift, true },
-    { "exec", op_exec, true },       { "set", op_set, true }
+    { "exec", op_exec, true },       { "set", op_set, true },       { "[", op_test, true },
+    { "test", op_test, true }
 };
 
 struct builtin_op *get_builtins() {
--- userland/sh/builtin.h
+++ userland/sh/builtin.h
@@ -3,7 +3,7 @@
 
 #include <stdbool.h>
 
-#define NUM_BUILTINS 23
+#define NUM_BUILTINS 25
 
 typedef int (*op_function_t)(char **args);
 
