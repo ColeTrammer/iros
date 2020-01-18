97c3b81c56a0d79fa2ae797eca49f97b1f73f71c
ColeTrammer <cole.trammer@gmail.com>
2020-01-17 19:12:59 -0800

    start: setuid and chdir before spawning shell

--- userland/start/main.c
+++ userland/start/main.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <fcntl.h>
+#include <pwd.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -8,9 +9,20 @@
 #include <termios.h>
 #include <unistd.h>
 
-void spawn_process(char **argv, bool redirect) {
+void spawn_process(char **argv, uid_t uid, bool redirect) {
     int pid = fork();
     if (pid == 0) {
+        if (uid != 0) {
+            if (setuid(uid)) {
+                perror("setuid");
+                exit(1);
+            }
+
+            if (chdir(getpwuid(uid)->pw_dir)) {
+                perror("chdir");
+                exit(1);
+            }
+        }
         setpgid(0, 0);
         if (redirect) {
             int dev_null = open("/dev/null", O_RDWR, 0);
@@ -31,21 +43,21 @@ void spawn_process(char **argv, bool redirect) {
 int main() {
     char *nslookup_args[] = { "/bin/nslookup", "-s", NULL };
 
-    spawn_process(nslookup_args, false);
+    spawn_process(nslookup_args, 0, false);
 #ifdef KERNEL_NO_GRAPHICS
     char *xterm_args[] = { "/bin/xterm", NULL };
 
-    spawn_process(xterm_args, false);
+    spawn_process(xterm_args, 100, false);
 #else
     char *window_server_args[] = { "/bin/window_server", NULL };
 
-    spawn_process(window_server_args, false);
+    spawn_process(window_server_args, 0, false);
 
     sleep(1);
 
     char *window_server_test_args[] = { "/initrd/window_server_test", NULL };
 
-    spawn_process(window_server_test_args, false);
+    spawn_process(window_server_test_args, 100, false);
 #endif /* KERNEL_NO_GRAPHICS */
 
     for (;;) {
