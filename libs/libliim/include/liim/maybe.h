e12bf4b26323587c3a27a0a02787693607a5d992
ColeTrammer <cole.trammer@gmail.com>
2020-01-29 16:50:52 -0800

    libliim: maybe move semantics

--- libs/libliim/include/liim/maybe.h
+++ libs/libliim/include/liim/maybe.h
@@ -3,6 +3,8 @@
 #include <assert.h>
 #include <liim/utilities.h>
 
+namespace LIIM {
+
 template<typename T> class Maybe {
 public:
     Maybe() {}
@@ -10,6 +12,13 @@ public:
     Maybe(const Maybe<T>& other) : m_has_value(other.has_value()) {
         if (other.has_value()) {
             new (&m_value[0]) T(other.value());
+        }
+    }
+    Maybe(Maybe<T>&& other) : m_has_value(other.has_value()) {
+        if (m_has_value) {
+            new (&m_value[0]) T(LIIM::move(*reinterpret_cast<T*>(&other.m_value[0])));
+            other.m_value().~T();
+            other.m_has_value = false;
         }
     }
 
@@ -36,6 +45,15 @@ public:
         return *this;
     }
 
+    Maybe<T>& operator=(Maybe&& other) {
+        if (this != &other) {
+            Maybe<T> temp(other);
+            swap(other);
+        }
+
+        return *this;
+    }
+
     bool has_value() const { return m_has_value; }
 
     T& value() {
@@ -44,7 +62,28 @@ public:
     }
     const T& value() const { return const_cast<Maybe<T>&>(*this).value(); }
 
+    void swap(Maybe<T>& other) {
+        if (this->has_value() && other.has_value()) {
+            LIIM::swap(this->value(), other.value());
+        } else if (this->has_value()) {
+            new (&other.m_value[0]) T(LIIM::move(*reinterpret_cast<T*>(&m_value[0])));
+            this->value().~T();
+        } else if (other.has_value()) {
+            new (&this->m_value[0]) T(LIIM::move(*reinterpret_cast<T*>(&m_value[0])));
+            other.value().~T();
+        }
+        LIIM::swap(this->m_has_value, other.m_has_value);
+    }
+
 private:
     char m_value[sizeof(T)];
     bool m_has_value { false };
 };
+
+template<typename T> void swap(Maybe<T>& a, Maybe<T>& b) {
+    a.swap(b);
+}
+
+}
+
+using LIIM::Maybe;
