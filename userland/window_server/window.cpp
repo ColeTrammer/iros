20c0157c177ae1e2e5d3b9d39c639fc1da5c336b
ColeTrammer <cole.trammer@gmail.com>
2020-02-17 15:42:01 -0800

    window_server: assert shared memory work

--- userland/window_server/window.cpp
+++ userland/window_server/window.cpp
@@ -15,11 +15,13 @@ Window::Window(String shm_path, const Rect& rect, int client_id)
     : m_shm_path(shm_path.string()), m_rect(rect), m_id(get_next_id()), m_client_id(client_id) {
     {
         int fd = shm_open(shm_path.string(), O_RDWR | O_CREAT | O_EXCL, 0666);
+        assert(fd != -1);
 
         size_t len = rect.width() * rect.height() * sizeof(uint32_t);
         ftruncate(fd, len);
 
         void* memory = mmap(nullptr, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        assert(memory != MAP_FAILED);
 
         m_front_buffer = PixelBuffer::wrap(reinterpret_cast<uint32_t*>(memory), m_rect.width(), m_rect.height());
         m_front_buffer->clear();
@@ -29,11 +31,13 @@ Window::Window(String shm_path, const Rect& rect, int client_id)
     {
         shm_path[shm_path.size() - 1]++;
         int fd = shm_open(shm_path.string(), O_RDWR | O_CREAT | O_EXCL, 0666);
+        assert(fd != -1);
 
         size_t len = rect.width() * rect.height() * sizeof(uint32_t);
         ftruncate(fd, len);
 
         void* memory = mmap(nullptr, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        assert(memory != MAP_FAILED);
 
         m_back_buffer = PixelBuffer::wrap(reinterpret_cast<uint32_t*>(memory), m_rect.width(), m_rect.height());
         m_back_buffer->clear();
