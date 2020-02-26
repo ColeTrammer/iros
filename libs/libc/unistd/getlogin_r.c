170c36189c470445b3b6eb1a4ad6d817ef12c1ce
ColeTrammer <cole.trammer@gmail.com>
2020-02-25 22:22:03 -0800

    libc: getlogin_r

--- libs/libc/unistd/getlogin_r.c
+++ libs/libc/unistd/getlogin_r.c
@@ -1,8 +1,32 @@
+#include <errno.h>
+#include <limits.h>
+#include <pwd.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 int getlogin_r(char *buf, size_t bufsize) {
-    (void) bufsize;
-    strcpy(buf, "root");
+    struct stat st;
+
+    // FIXME: this should tecnically use /dev/tty and not stdin
+    if (fstat(STDIN_FILENO, &st) < 0) {
+        errno = ENXIO;
+        return -1;
+    }
+
+    struct passwd pwd;
+    struct passwd *result;
+    char pwd_buf[1024];
+    if (!getpwuid_r(st.st_uid, &pwd, pwd_buf, sizeof(pwd_buf), &result)) {
+        return -1;
+    }
+
+    size_t len = strlen(result->pw_name) + 1;
+    if (len > bufsize) {
+        errno = ERANGE;
+        return -1;
+    }
+
+    strcpy(buf, result->pw_name);
     return 0;
 }
