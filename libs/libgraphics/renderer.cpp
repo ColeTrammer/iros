960a31ec57f9296a678d53faf00e0a2a9c67ea34
ColeTrammer <cole.trammer@gmail.com>
2020-03-11 21:05:33 -0700

    libgraphics: fix render_text to not go out of bounds
    This means that xterm can scroll and clear the screen.

--- libs/libgraphics/renderer.cpp
+++ libs/libgraphics/renderer.cpp
@@ -55,7 +55,7 @@ void Renderer::render_text(int x, int y, const String& text) {
     for (int k = 0; k < text.size(); k++) {
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 8; j++) {
-                m_pixels.put_pixel(k * 8 + x - j + 8, y + i,
+                m_pixels.put_pixel(k * 8 + x - j + 7, y + i,
                                    font.get_for_character(text[k])->get(i * 8 + j) ? color().color() : 0xFF000000);
             }
         }
