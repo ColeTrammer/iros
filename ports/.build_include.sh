6db51623535f62ec2d5764b182c34e2ee8f4af95
ColeTrammer <cole.trammer@gmail.com>
2020-03-25 22:38:29 -0700

    ports: start work on a smarter port system
    This file acts as a driver for the port's ./build.sh script, and should
    make using ./build.sh much more convenient.
    
    Porting a project can be separated into a series of steps, each of which
    can be done individually by the port script. These include things like
    downloading, cleaning, installing, and building.
    
    This will make it much easier to do things like reinstall a port that's
    already been built, which happens a lot since the default disk image
    size is very stall, and writing useless ports to the disk is a waste
    of time.
    
    This first iteration provides no defaults for steps like configure,
    which sometimes just do nothing, and install, which almost always
    just does make install DEST_DIR=$ROOT/sysroot/usr.

---
+++ ports/.build_include.sh
@@ -0,0 +1,91 @@
+set -e
+
+export ROOT="$PWD/../.."
+export HOST=`$ROOT/default-host.sh`
+
+die() {
+    echo "Error:" "$@" && exit 1
+}
+
+list_target() {
+    echo "List of targets:"
+    echo "  download: download and unzip files"
+    echo "  patch: change the files to work"
+    echo "  configure: prepare for build"
+    echo "  build: compile the port"
+    echo "  install: install port to sysroot"
+    echo "  all (default): all of the above"
+    echo "  clean: delete build files"
+}
+
+if [ ! $PORT_NAME ] || [ ! $SRC_DIR ] || [ ! $BUILD_DIR ]; then
+    die "incorrect invocation of .build_include.sh"
+fi
+
+TARGET=${1:-all}
+if [ $TARGET = '--help' ]; then
+    echo "Usage:" "./build.sh" "[target]"
+    list_targets
+    exit 0
+fi
+
+fake_pushd() {
+    export __OLD_PUSHD_DIRECTORY=$PWD
+    cd $1
+}
+
+fake_popd() {
+    cd $__OLD_PUSHD_DIRECTORY
+}
+
+run() {
+    case $TARGET in
+        all)
+            download || die "download failed"
+            patch || die "patch failed"
+            configure || die "configure failed"
+            build || die "build failed"
+            install || die "install failed"
+            ;;
+        clean)
+            fake_pushd $BUILD_DIR
+            clean || die "clean failed"
+            fake_popd
+            ;;
+        install)
+            fake_pushd $BUILD_DIR
+            install || die "install failed"
+            fake_popd
+            ;;
+        build)
+            fake_pushd $BUILD_DIR
+            build || die "build failed"
+            fake_popd
+            ;;
+        download)
+            download || die "download failed"
+            ;;
+        patch)
+            fake_pushd $SRC_DIR
+            patch || die "patch failed"
+            fake_popd
+            ;;
+        configure)
+            if [ ! -e $BUILD_DIR ]; then
+                fake_pushd ../..
+                make install-headers
+                fake_popd
+
+                mkdir -p $BUILD_DIR
+                fake_pushd $BUILD_DIR
+                configure || die "configure failed"
+                fake_popd
+            fi
+            ;;
+        *)
+            die "unrecognized target:" $TARGET
+            ;;
+    esac
+}
+
+run
