8d9d5a3ed90a416c2f59017211b0aeb1bfbe96b7
ColeTrammer <cole.trammer@gmail.com>
2020-03-26 22:47:49 -0700

    libliim: add the clamp function

--- libs/libliim/include/liim/utilities.h
+++ libs/libliim/include/liim/utilities.h
@@ -475,12 +475,23 @@ void swap(T& a, T& b) {
 }
 
 template<typename T>
-const T& max(const T& a, const T& b) {
+constexpr const T& clamp(const T& t, const T& min, const T& max) {
+    if (t < min) {
+        return min;
+    } else if (t > max) {
+        return max;
+    }
+
+    return t;
+}
+
+template<typename T>
+constexpr const T& max(const T& a, const T& b) {
     return a >= b ? a : b;
 }
 
 template<typename T>
-const T& min(const T& a, const T& b) {
+constexpr const T& min(const T& a, const T& b) {
     return a <= b ? a : b;
 }
 
