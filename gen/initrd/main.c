bcbaa9c31a5a5bf4700eeaae23a8570501e6a819
ColeTrammer <cole.trammer@gmail.com>
2020-04-05 22:02:06 -0700

    initrd: allow calling the program with an "exclude" argument
    This will allow calling the program on the cmake output directory
    directly.

--- gen/initrd/main.c
+++ gen/initrd/main.c
@@ -1,9 +1,12 @@
 #include <dirent.h>
+#include <regex.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define MAX_FILE_NAME_LENGTH 64
 
@@ -27,27 +30,49 @@ int64_t count_files(char *dir) {
     return -1;
 }
 
+void print_usage_and_exit(const char *name) {
+    fprintf(stderr, "Usage: %s [-e exclude-file] <dir> <output>\n", name);
+    exit(2);
+}
+
 int main(int argc, char **argv) {
-    if (argc < 3) {
-        printf("Usage: %s <dir> <outfile>\n", argv[0]);
-        return 1;
+    int opt;
+    char *exclude = NULL;
+
+    while ((opt = getopt(argc, argv, ":e:")) != -1) {
+        switch (opt) {
+            case 'e':
+                exclude = optarg;
+                break;
+            case ':':
+            case '?':
+                print_usage_and_exit(*argv);
+                break;
+        }
     }
 
-    int64_t files = count_files(argv[1]);
+    if (argc - optind < 2) {
+        print_usage_and_exit(*argv);
+    }
+
+    char *dir = argv[optind];
+    char *output = argv[optind + 1];
+
+    int64_t files = count_files(dir);
     if (files <= 0) {
-        printf("No Files in Dir: %s\n", argv[1]);
+        printf("No Files in Dir: %s\n", dir);
         return 1;
     }
     printf("Process %ld file(s)...\n", files);
 
-    size_t dir_name_length = strlen(argv[1]) + 1;
+    size_t dir_name_length = strlen(dir) + 1;
     char *file_name = calloc(dir_name_length + MAX_FILE_NAME_LENGTH, 1);
-    strcpy(file_name, argv[1]);
+    strcpy(file_name, dir);
     file_name[dir_name_length - 1] = '/';
 
-    FILE *initrd = fopen(argv[2], "w");
+    FILE *initrd = fopen(output, "w");
     if (!initrd) {
-        printf("Invalid outfile: %s\n", argv[2]);
+        printf("Invalid outfile: %s\n", output);
         return 1;
     }
 
@@ -55,7 +80,7 @@ int main(int argc, char **argv) {
 
     size_t *file_lengths = calloc(files, sizeof(size_t));
 
-    DIR *d = opendir(argv[1]);
+    DIR *d = opendir(dir);
     if (d) {
         struct initrd_file_entry entry;
         uint32_t offset = sizeof(int64_t) + files * sizeof(struct initrd_file_entry);
@@ -63,7 +88,14 @@ int main(int argc, char **argv) {
         struct dirent *dir;
         int i = 0;
         while ((dir = readdir(d)) != NULL) {
-            if (strcmp(dir->d_name, ".") && strcmp(dir->d_name, "..")) {
+            struct stat st;
+            if (fstatat(dirfd(d), dir->d_name, &st, 0)) {
+                perror("fstatat");
+                return 1;
+            }
+
+            if (S_ISREG(st.st_mode) && strcmp(dir->d_name, ".") && strcmp(dir->d_name, "..") &&
+                (!exclude || strcmp(dir->d_name, exclude))) {
                 if (strlen(dir->d_name) > MAX_FILE_NAME_LENGTH) {
                     printf("File Name Too Long: %s\n", dir->d_name);
                     return 1;
@@ -97,12 +129,19 @@ int main(int argc, char **argv) {
         closedir(d);
     }
 
-    d = opendir(argv[1]);
+    d = opendir(dir);
     if (d) {
         struct dirent *dir;
         int i = 0;
         while ((dir = readdir(d)) != NULL) {
-            if (strcmp(dir->d_name, ".") && strcmp(dir->d_name, "..")) {
+            struct stat st;
+            if (fstatat(dirfd(d), dir->d_name, &st, 0)) {
+                perror("fstatat");
+                return 1;
+            }
+
+            if (S_ISREG(st.st_mode) && strcmp(dir->d_name, ".") && strcmp(dir->d_name, "..") &&
+                (!exclude || strcmp(dir->d_name, exclude))) {
                 size_t buffer_length = (file_lengths[i] & ~7) + 8;
                 uint8_t *buffer = calloc(buffer_length, sizeof(uint8_t));
 
