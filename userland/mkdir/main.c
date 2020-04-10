0271f5af5cf00026916a91b3fb6e1d1027833445
ColeTrammer <cole.trammer@gmail.com>
2020-04-09 20:41:30 -0700

    mkdir: support the -p flag
    Extremely simple implementation using chdir and strtok("/").

--- userland/mkdir/main.c
+++ userland/mkdir/main.c
@@ -1,16 +1,96 @@
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        printf("Usage: %s <dir-name>]n", argv[0]);
+int do_mkdir(char *path, mode_t mode, bool make_parents) {
+    if (!make_parents) {
+        int ret = mkdir(path, mode);
+        if (ret != 0) {
+            perror("mkdir");
+            return 1;
+        }
+
         return 0;
     }
 
-    int ret = mkdir(argv[1], 0777);
-    if (ret != 0) {
-        perror("mkdir");
+    int cwd_save = open(".", O_DIRECTORY);
+    if (cwd_save == -1) {
+        perror("open");
         return 1;
+    }
+
+    if (*path == '/') {
+        if (chdir("/")) {
+            perror("chidr");
+            return 1;
+        }
+    }
+
+    char *component = strtok(path, "/");
+    while (component) {
+        int ret = mkdir(component, mode);
+        if (ret != 0 && errno != EEXIST) {
+            perror("mkdir");
+            return 1;
+        }
+
+        if (chdir(component)) {
+            perror("chdir");
+            return 1;
+        }
+
+        component = strtok(NULL, "/");
+    }
+
+    if (fchdir(cwd_save)) {
+        perror("fchdir");
+        return 1;
+    }
+
+    if (close(cwd_save)) {
+        perror("close");
+        return 1;
+    }
+
+    return 0;
+}
+
+void print_usage_and_exit(const char *s) {
+    fprintf(stderr, "Usage: %s [-p] <dirs...>\n", s);
+    exit(2);
+}
+
+int main(int argc, char **argv) {
+    int opt;
+    bool p = false;
+    mode_t mode = 0777;
+    while ((opt = getopt(argc, argv, ":p")) != -1) {
+        switch (opt) {
+            case 'p':
+                p = true;
+                break;
+            case ':':
+            case '?':
+                print_usage_and_exit(*argv);
+                break;
+        }
+    }
+
+    if (optind == argc) {
+        print_usage_and_exit(*argv);
+    }
+
+    for (; optind < argc; optind++) {
+        int ret = do_mkdir(argv[optind], mode, p);
+        if (ret) {
+            return ret;
+        }
     }
 
     return 0;
