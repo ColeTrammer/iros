e4ef1f262beaad0620db217f4705a7c7feffc841
ColeTrammer <cole.trammer@gmail.com>
2020-04-18 17:19:34 -0700

    libc: really fix ftell()
    ftell() actually has different behavior based on whether the last
    operation was a read or a write. When doing a read, the return of
    fseek(SEEK_CUR) is past where it should be, while when doing a write,
    fseek(SEEK_CUR) returns too small of a value.

--- libs/libc/stdio/ftell.c
+++ libs/libc/stdio/ftell.c
@@ -8,20 +8,31 @@
 
 long ftell(FILE *stream) {
     __lock(&stream->__lock);
-    off_t ret = stream->__position;
 
-    // NOTE: this is has very strange consequences when the actual file position is 0
-    if (stream->__flags & __STDIO_HAS_UNGETC_CHARACTER) {
-        ret--;
-    }
+    off_t ret = 0;
+    if (stream->__flags & __STDIO_LAST_OP_WRITE) {
+        off_t res = lseek(stream->__fd, 0, SEEK_CUR);
+        if (res == -1) {
+            ret = -1;
+        } else {
+            ret = res + stream->__position;
+        }
+    } else if (stream->__flags & __STDIO_LAST_OP_READ) {
+        // NOTE: this is has very strange consequences when the actual file position is 0
+        if (stream->__flags & __STDIO_HAS_UNGETC_CHARACTER) {
+            ret--;
+        }
 
-    off_t res = lseek(stream->__fd, 0, SEEK_CUR);
-    if (res == -1) {
-        ret = -1;
-    } else if (res == 0) {
-        ret = MAX(0, res + ret);
+        off_t res = lseek(stream->__fd, 0, SEEK_CUR);
+        if (res == -1) {
+            ret = -1;
+        } else if (res == 0) {
+            ret = 0;
+        } else {
+            ret += res - stream->__buffer_length + stream->__position;
+        }
     } else {
-        ret += res;
+        ret = lseek(stream->__fd, 0, SEEK_CUR);
     }
 
     __unlock(&stream->__lock);
