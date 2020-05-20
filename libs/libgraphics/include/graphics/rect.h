2667ba7792704621b6782f78d7ee0d5313eda2f0
ColeTrammer <cole.trammer@gmail.com>
2020-05-19 17:27:02 -0700

    libgraphics: don't define unecessary Rect constructor/destructor

--- libs/libgraphics/include/graphics/rect.h
+++ libs/libgraphics/include/graphics/rect.h
@@ -8,8 +8,6 @@ public:
 
     Rect(int x, int y, int width, int height) : m_x(x), m_y(y), m_width(width), m_height(height) {}
 
-    Rect(const Rect& other) : m_x(other.x()), m_y(other.y()), m_width(other.width()), m_height(other.height()) {}
-
     int x() const { return m_x; }
     int y() const { return m_y; }
     int width() const { return m_width; }
@@ -23,8 +21,6 @@ public:
     Point center() const { return Point(x() + width() / 2, y() + height() / 2); }
 
     bool intersects(Point p) const { return p.x() >= m_x && p.x() <= m_x + m_width && p.y() >= m_y && p.y() <= m_y + m_height; }
-
-    ~Rect() {}
 
 private:
     int m_x { 0 };
