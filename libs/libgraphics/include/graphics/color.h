4e083fbfb546e23ed536cbc27646e2bc480b0ba3
ColeTrammer <cole.trammer@gmail.com>
2020-05-30 22:53:59 -0700

    libgraphics: fix Color constructor
    For whatever reason, its arguments used to be r b g instead of
    r g b.

--- libs/libgraphics/include/graphics/color.h
+++ libs/libgraphics/include/graphics/color.h
@@ -8,7 +8,7 @@ class Color {
 public:
     Color() : Color(0xFF, 0xFF, 0xFF) {}
 
-    Color(uint8_t r, uint8_t b, uint8_t g, uint8_t a = 0x00) : m_color(a << 24 | r << 16 | g << 8 | b) {}
+    Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0x00) : m_color(a << 24 | r << 16 | g << 8 | b) {}
     Color(enum vga_color color);
     Color(uint32_t value) : m_color(value) {}
 
@@ -16,7 +16,7 @@ public:
 
     uint32_t color() const { return m_color; }
 
-    void set(uint8_t r, uint8_t b, uint8_t g, uint8_t a = 0x00) { m_color = a << 24 | r << 16 | g << 8 | b; }
+    void set(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0x00) { m_color = a << 24 | r << 16 | g << 8 | b; }
 
     uint8_t a() const { return m_color & 0xFF000000U; }
     uint8_t r() const { return m_color & 0x00FF0000U; }
