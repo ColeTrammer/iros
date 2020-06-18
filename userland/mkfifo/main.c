8cc3f383a2081ed68b91b0fed1c1780dbeb47226
ColeTrammer <cole.trammer@gmail.com>
2020-06-17 21:35:40 -0700

    userland: add mkfifo utility

---
+++ userland/mkfifo/main.c
@@ -0,0 +1,42 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void print_usage_and_exit(const char *s) {
+    fprintf(stderr, "Usage: %s [-m mode] <names...>\n", s);
+    exit(2);
+}
+
+int main(int argc, char **argv) {
+    mode_t mode = 0666;
+
+    int opt;
+    while ((opt = getopt(argc, argv, ":m:")) != -1) {
+        switch (opt) {
+            case 'm':
+                break;
+            case ':':
+            case '?':
+                print_usage_and_exit(*argv);
+                break;
+        }
+    }
+
+    if (optind == argc) {
+        print_usage_and_exit(*argv);
+    }
+
+    bool any_failed = false;
+    for (; optind < argc; optind++) {
+        if (mkfifo(argv[optind], mode)) {
+            fprintf(stderr, "%s: Failed to create `%s': %s\n", *argv, argv[optind], strerror(errno));
+            any_failed = true;
+        }
+    }
+
+    return any_failed;
+}
