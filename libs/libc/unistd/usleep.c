519efc2293f8c1a611140fc9bcfab684f9de840b
ColeTrammer <cole.trammer@gmail.com>
2020-06-24 18:14:20 -0700

    libc: fix usleep's conversion to a timespec

--- libs/libc/unistd/usleep.c
+++ libs/libc/unistd/usleep.c
@@ -2,6 +2,6 @@
 #include <unistd.h>
 
 int usleep(useconds_t usec) {
-    struct timespec t = { .tv_sec = usec / 1000000, .tv_nsec = usec * 1000 };
+    struct timespec t = { .tv_sec = usec / 1000000, .tv_nsec = (usec & 1000000) * 1000 };
     return nanosleep(&t, NULL);
 }
