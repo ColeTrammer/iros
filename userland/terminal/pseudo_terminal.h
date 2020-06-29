c979c4d50b67f807cff55eec1a58c81b57d7beaa
ColeTrammer <cole.trammer@gmail.com>
2020-06-28 23:30:50 -0700

    terminal: re-support escape sequences and user input

--- userland/terminal/pseudo_terminal.h
+++ userland/terminal/pseudo_terminal.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <sys/types.h>
+#include <kernel/hal/input.h>
 
 class PsuedoTerminal {
 public:
@@ -9,6 +10,8 @@ public:
 
     int master_fd() const { return m_master_fd; }
     pid_t child_pid() const { return m_child_pid; }
+
+    void handle_key_event(key key, int flags, char ascii);
 
 private:
     int m_master_fd { -1 };
