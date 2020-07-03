7ba0e2f79079a0e3ca91fc5b09fbe2c8837a8d1d
ColeTrammer <cole.trammer@gmail.com>
2020-07-02 23:37:05 -0700

    kernel: add way to debug wait queues

--- kernel/include/kernel/proc/wait_queue.h
+++ kernel/include/kernel/proc/wait_queue.h
@@ -16,14 +16,18 @@ struct wait_queue {
 #define WAIT_QUEUE_INITIALIZER \
     { SPINLOCK_INITIALIZER, NULL, NULL }
 
-void init_wait_queue(struct wait_queue *queue);
+void __wait_queue_enqueue_task(struct wait_queue *queue, struct task *task, const char *func);
+void __wake_up_n(struct wait_queue *queue, int n, const char *func);
 
-void __wait_queue_enqueue_task(struct wait_queue *queue, struct task *task);
-void __wake_up_n(struct wait_queue *queue, int n);
+void init_wait_queue_internal(struct wait_queue *queue, const char *func);
+void wait_on_internal(struct wait_queue *queue, const char *func);
+void wake_up_internal(struct wait_queue *queue, const char *func);
+void wake_up_all_internal(struct wait_queue *queue, const char *func);
 
-void wait_on(struct wait_queue *queue);
-void wake_up(struct wait_queue *queue);
-void wake_up_all(struct wait_queue *queue);
+#define init_wait_queue(w) init_wait_queue_internal(w, __func__)
+#define wait_on(w)         wait_on_internal(w, __func__)
+#define wake_up(w)         wake_up_internal(w, __func__)
+#define wake_up_all(w)     wake_up_all_internal(w, __func__)
 
 static inline __attribute__((always_inline)) bool __wait_queue_is_empty(struct wait_queue *queue) {
     return queue->waiters_head == NULL;
--- kernel/proc/wait_queue.c
+++ kernel/proc/wait_queue.c
@@ -5,12 +5,17 @@
 
 // #define WAIT_QUEUE_DEBUG
 
-void init_wait_queue(struct wait_queue *queue) {
+void init_wait_queue_internal(struct wait_queue *queue, const char *func) {
+    (void) func;
     init_spinlock(&queue->lock);
     queue->waiters_head = queue->waiters_tail = NULL;
+#ifdef WAIT_QUEUE_DEBUG
+    debug_log("Initialized wait queue: [ %p, %s ]\n", queue, func);
+#endif /* WAIT_QUEUE_DEBUG */
 }
 
-void __wait_queue_enqueue_task(struct wait_queue *queue, struct task *task) {
+void __wait_queue_enqueue_task(struct wait_queue *queue, struct task *task, const char *func) {
+    (void) func;
     assert(task->wait_queue_next == NULL);
     if (!queue->waiters_head) {
         queue->waiters_head = queue->waiters_tail = task;
@@ -20,17 +25,18 @@ void __wait_queue_enqueue_task(struct wait_queue *queue, struct task *task) {
     }
 
 #ifdef WAIT_QUEUE_DEBUG
-    debug_log("enqueuing task: [ %p, %d:%d ]\n", queue, task->process->pid, task->tid);
+    debug_log("enqueuing task: [ %p, %d:%d, %s ]\n", queue, task->process->pid, task->tid, func);
 #endif /* WAIT_QUEUE_DEBUG */
 }
 
-bool __wake_up(struct wait_queue *queue) {
+bool __wake_up(struct wait_queue *queue, const char *func) {
+    (void) func;
     struct task *to_wake = queue->waiters_head;
     if (to_wake) {
         assert(to_wake->sched_state != EXITING);
         to_wake->sched_state = RUNNING_UNINTERRUPTIBLE;
 #ifdef WAIT_QUEUE_DEBUG
-        debug_log("waking up task: [ %p, %d:%d ]\n", queue, to_wake->process->pid, to_wake->tid);
+        debug_log("waking up task: [ %p, %d:%d, %s ]\n", queue, to_wake->process->pid, to_wake->tid, func);
 #endif /* WAIT_QUEUE_DEBUG */
 
         queue->waiters_head = to_wake->wait_queue_next;
@@ -46,20 +52,20 @@ bool __wake_up(struct wait_queue *queue) {
     return false;
 }
 
-void __wake_up_n(struct wait_queue *queue, int n) {
+void __wake_up_n(struct wait_queue *queue, int n, const char *func) {
     for (int i = 0; i < n; i++) {
-        if (!__wake_up(queue)) {
+        if (!__wake_up(queue, func)) {
             break;
         }
     }
 }
 
-void wait_on(struct wait_queue *queue) {
+void wait_on_internal(struct wait_queue *queue, const char *func) {
     struct task *task = get_current_task();
     task->wait_queue_next = NULL;
 
     spin_lock(&queue->lock);
-    __wait_queue_enqueue_task(queue, task);
+    __wait_queue_enqueue_task(queue, task, func);
 
     task->sched_state = WAITING;
     spin_unlock(&queue->lock);
@@ -67,22 +73,23 @@ void wait_on(struct wait_queue *queue) {
     __kernel_yield();
 }
 
-void wake_up(struct wait_queue *queue) {
+void wake_up_internal(struct wait_queue *queue, const char *func) {
     spin_lock(&queue->lock);
 
-    __wake_up(queue);
+    __wake_up(queue, func);
 
     spin_unlock(&queue->lock);
 }
 
-void wake_up_all(struct wait_queue *queue) {
+void wake_up_all_internal(struct wait_queue *queue, const char *func) {
+    (void) func;
     spin_lock(&queue->lock);
 
     struct task *to_wake = queue->waiters_head;
     while (to_wake) {
         to_wake->sched_state = RUNNING_UNINTERRUPTIBLE;
 #ifdef WAIT_QUEUE_DEBUG
-        debug_log("waking up task: [ %p, %d:%d ]\n", queue, to_wake->process->pid, to_wake->tid);
+        debug_log("waking up task: [ %p, %d:%d, %s ]\n", queue, to_wake->process->pid, to_wake->tid, func);
 #endif /* WAIT_QUEUE_DEBUG */
         to_wake = to_wake->wait_queue_next;
     }
