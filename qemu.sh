5003fc3636f53507d2db984acc26f5ade33cbc85
ColeTrammer <cole.trammer@gmail.com>
2020-07-07 01:47:24 -0700

    qemu.sh: update
    Specify the disk image drive type (to avoid warnings), ignore cpu
    resets (sending an SIPI to a CPU triggers them), and remove packet
    capture code which is unecessary now the kernel's network code kind
    of works.

--- qemu.sh
+++ qemu.sh
@@ -13,14 +13,13 @@ ARCH=${ARCH:-x86_64}
 
 qemu-system-$ARCH \
     -cdrom os_2.iso \
-    -d cpu_reset,guest_errors \
+    -d guest_errors \
     -serial stdio \
     -smp 2 \
     -device VGA,vgamem_mb=64 \
-    -hda os_2.img \
+    -drive file=os_2.img,format=raw,index=0,media=disk \
     -boot d \
     -no-reboot \
     -no-shutdown \
-    -object filter-dump,id=hue,netdev=breh,file=e1000.pcap \
     -netdev user,id=breh,hostfwd=tcp:127.0.0.1:8888-10.0.2.15:8888,hostfwd=tcp:127.0.0.1:8823-10.0.2.15:23 \
     -device e1000,netdev=breh
