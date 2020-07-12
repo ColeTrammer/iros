b8de8249554644c69f89233386ec5626c23b3ef0
ColeTrammer <cole.trammer@gmail.com>
2020-07-12 02:25:55 -0700

    liim: added FixedArray class
    This class is very useful when there is a compile time maximum
    number of elements in an array, but at run time the actual number
    of elements can be less than this compile time maximum. This is
    pretty much a thin wrapper around an actual T[].

---
+++ libs/libliim/include/liim/fixed_array.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <liim/utilities.h>
+
+template<typename T, size_t max_elements>
+class FixedArray {
+public:
+    constexpr FixedArray(size_t size) : m_size(size) { assert(size <= max_elements); }
+
+    constexpr T& operator[](size_t index) {
+        assert(index < m_size);
+        return m_array[index];
+    }
+
+    constexpr const T& operator[](size_t index) const {
+        assert(index < m_size);
+        return m_array[index];
+    }
+
+    constexpr size_t size() const { return m_size; }
+
+    constexpr T* array() { return m_array; }
+    constexpr const T* array() const { return m_array; }
+
+private:
+    size_t m_size { 0 };
+    T m_array[max_elements];
+};
