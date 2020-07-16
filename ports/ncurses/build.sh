c50521846baecc9afeb236bbadd5f19c34834be7
ColeTrammer <cole.trammer@gmail.com>
2020-07-16 02:43:19 -0700

    ports: add ncurses port

---
+++ ports/ncurses/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+PORT_NAME=ncurses
+SRC_DIR='ncurses-6.2'
+BUILD_DIR='build-ncurses'
+
+download() {
+    # Download tar.gz
+    curl ftp://ftp.invisible-island.net/ncurses/ncurses.tar.gz --output ncurses-6.2.tar.gz
+
+    # Extract contents
+    tar -xzvf ncurses-6.2.tar.gz
+}
+
+patch() {
+    git init
+    git apply ../ncurses-6.2.patch
+}
+
+configure() {
+    ../"$SRC_DIR"/configure --host=$HOST --prefix=/usr --with-termlib
+}
+
+. ../.build_include.sh
