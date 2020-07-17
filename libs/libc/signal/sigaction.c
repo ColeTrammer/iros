561cbc40df0f306cf54526299f7bad84056f60a3
ColeTrammer <cole.trammer@gmail.com>
2020-07-17 01:38:52 -0700

    libc: fix null dereference in sigaction

--- libs/libc/signal/sigaction.c
+++ libs/libc/signal/sigaction.c
@@ -3,7 +3,9 @@
 #include <sys/syscall.h>
 
 int sigaction(int signum, const struct sigaction *act, struct sigaction *old_act) {
-    ((struct sigaction *) act)->sa_restorer = &__sigreturn;
+    if (act) {
+        ((struct sigaction *) act)->sa_restorer = &__sigreturn;
+    }
     int ret = (int) syscall(SC_SIGACTION, signum, act, old_act);
     __SYSCALL_TO_ERRNO(ret);
 }
