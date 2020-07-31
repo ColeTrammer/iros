2753f538aa381df47c3653b67838bf9440234da7
ColeTrammer <cole.trammer@gmail.com>
2020-07-31 00:37:43 -0700

    toolchain: don't bother with libitm

--- toolchain/gcc-9.2.0/build-libstdc++-v3.sh
+++ toolchain/gcc-9.2.0/build-libstdc++-v3.sh
@@ -23,12 +23,6 @@ cd build-gcc
 make all-target-libstdc++-v3 -j5
 make install-target-libstdc++-v3
 
-cd "$TARGET/libitm"
-make -j5
-make install
-cd ../..
-
 cp "$ROOT/toolchain/cross/x86_64-os_2/lib/libstdc++.so.6.0.27" "$ROOT/base/usr/lib"
-cp "$ROOT/toolchain/cross/x86_64-os_2/lib/libitm.so.1.0.0" "$ROOT/base/usr/lib"
 
 cd ..
