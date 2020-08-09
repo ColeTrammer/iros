375e44862ab7d371d90d16692ac75555b6301e06
ColeTrammer <cole.trammer@gmail.com>
2020-08-09 11:42:07 -0700

    libc: fix up setvbuf a bit

--- libs/libc/stdio/setvbuf.c
+++ libs/libc/stdio/setvbuf.c
@@ -7,26 +7,40 @@ int setvbuf(FILE *__restrict stream, char *__restrict buf, int mode, size_t size
         return -1;
     }
 
+    int ret = 0;
+    __lock(&stream->__lock);
     __stdio_log(stream, "setvbuf: [ %d, %p, %d, %lu ]\n", stream->__fd, buf, mode, size);
 
-    fflush(stream);
+    fflush_unlocked(stream);
+
+    // Check case where only the buffer mode is being changed.
+    if ((stream->__flags & __STDIO_OWNED) && (buf == NULL) && (size == stream->__buffer_max)) {
+        goto update_flags;
+    }
 
     if (stream->__flags & __STDIO_OWNED) {
         free(stream->__buffer);
+        stream->__flags &= ~__STDIO_OWNED;
+        stream->__buffer = NULL;
+        stream->__buffer_max = 0;
     }
 
-    stream->__flags &= ~__STDIO_OWNED;
     if (buf == NULL) {
         buf = malloc(size);
         if (!buf) {
-            return -1;
+            ret = -1;
+            mode = _IONBF; /* Set mode to _IONBF since we no longer have one */
+            goto update_flags;
         }
         stream->__flags |= __STDIO_OWNED;
     }
 
     stream->__buffer = buf;
     stream->__buffer_max = size;
+
+update_flags:
     stream->__flags &= ~(_IOFBF | _IONBF | _IOLBF);
     stream->__flags |= mode;
-    return 0;
+    __unlock(&stream->__lock);
+    return ret;
 }
