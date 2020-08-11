6db915fef059ec86ae6fa57d5b2d8dd3de8c900b
ColeTrammer <cole.trammer@gmail.com>
2020-08-11 00:04:22 -0700

    libext: remove perror() in library call

--- libs/libext/mapped_file.cpp
+++ libs/libext/mapped_file.cpp
@@ -9,26 +9,22 @@ namespace Ext {
 UniquePtr<MappedFile> MappedFile::create(const String& path, int prot, int type) {
     int fd = open(path.string(), O_RDONLY);
     if (fd == -1) {
-        perror("open");
         return nullptr;
     }
 
     struct stat st;
     if (fstat(fd, &st)) {
-        perror("fstat");
         close(fd);
         return nullptr;
     }
 
     uint8_t* raw_data = (uint8_t*) mmap(nullptr, st.st_size, prot, type, fd, 0);
     if (raw_data == MAP_FAILED) {
-        perror("mmap");
         close(fd);
         return nullptr;
     }
 
     if (close(fd)) {
-        perror("close");
         munmap(raw_data, st.st_size);
         return nullptr;
     }
