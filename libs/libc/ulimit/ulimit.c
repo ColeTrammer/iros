130519b8e91cc171bc017291eaa7784b1b99db27
ColeTrammer <cole.trammer@gmail.com>
2020-08-12 12:14:08 -0700

    libc: add a ulimit implemenation that calls get/setrlimit

--- libs/libc/ulimit/ulimit.c
+++ libs/libc/ulimit/ulimit.c
@@ -1,7 +1,35 @@
-#include <stdio.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <sys/resource.h>
 #include <ulimit.h>
 
+#define ULIMIT_TO_RLIMIT_FACTOR 512
+
 long ulimit(int cmd, ...) {
-    fprintf(stderr, "ulimit not implemented: %d\n", cmd);
-    return 0;
+    switch (cmd) {
+        case UL_GETFSIZE: {
+            struct rlimit lim;
+            int ret = getrlimit(RLIMIT_FSIZE, &lim);
+            if (ret) {
+                return ret;
+            }
+            return lim.rlim_cur / ULIMIT_TO_RLIMIT_FACTOR;
+        }
+        case UL_SETFSIZE: {
+            va_list args;
+            va_start(args, cmd);
+            long limit = va_arg(args, long);
+            va_end(args);
+            struct rlimit lim;
+            int ret = getrlimit(RLIMIT_FSIZE, &lim);
+            if (ret) {
+                return ret;
+            }
+            lim.rlim_cur = limit * ULIMIT_TO_RLIMIT_FACTOR;
+            return setrlimit(RLIMIT_FSIZE, &lim);
+        }
+        default:
+            errno = EINVAL;
+            return -1;
+    }
 }
