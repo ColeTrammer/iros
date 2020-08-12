deb2022bb8e4f455a02f4de12820ec0b8aa6497e
ColeTrammer <cole.trammer@gmail.com>
2020-08-12 14:41:19 -0700

    libc: change fgetpos() to call ftell()
    Since fsetpos() calls fseek(), fgetpos() should call ftell()

--- libs/libc/stdio/fgetpos.c
+++ libs/libc/stdio/fgetpos.c
@@ -3,14 +3,9 @@
 #include <unistd.h>
 
 int fgetpos(FILE *__restrict stream, fpos_t *__restrict pos) {
-    __lock(&stream->__lock);
-    off_t ret = lseek(stream->__fd, 0, SEEK_CUR);
-    __unlock(&stream->__lock);
-
+    long ret = ftell(stream);
     if (ret >= 0) {
         *pos = ret;
-        return 0;
     }
-
-    return -1;
+    return ret;
 }
