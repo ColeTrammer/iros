193c8cbea939ae1e4a0f70ef649270b2bad64a49
ColeTrammer <cole.trammer@gmail.com>
2020-08-26 14:59:24 -0700

    libpthread: make declarations not weak
    It turns out the pthread_* symbols don't really need to be weak b/c
    gcc makes weak aliases for them.

--- initrd/programs/test_cpp_a.cpp
+++ initrd/programs/test_cpp_a.cpp
@@ -1,11 +1,29 @@
+#include <chrono>
 #include <iostream>
+#include <mutex>
+#include <thread>
 #include <vector>
+
+using namespace std::chrono_literals;
 
 std::vector ints { 0, 1, 2 };
 
 int main() {
     double x = 1.123;
     std::cout << x << std::endl;
+
+    std::mutex m;
+    auto t = std::thread([&] {
+        std::lock_guard<std::mutex> guard(m);
+        printf("T1\n");
+    });
+
+    std::this_thread::sleep_for(100ms);
+    {
+        std::lock_guard<std::mutex> guard(m);
+        printf("T2\n");
+    }
+    t.join();
 
     std::cout << ints.at(4) << std::endl;
 
--- libs/libpthread/once.c
+++ libs/libpthread/once.c
@@ -14,9 +14,13 @@ int pthread_once(pthread_once_t *once_control, void (*init_routine)(void)) {
     for (;;) {
         int expect = 0;
         if (atomic_compare_exchange_strong(once_control, &expect, __PTHREAD_ONCE_IN_PROGRESS) != 0) {
-            pthread_cleanup_push((void (*)(void *)) & reset_once, once_control);
-            (*init_routine)();
-            pthread_cleanup_pop(0);
+            if (__threads) {
+                pthread_cleanup_push((void (*)(void *)) & reset_once, once_control);
+                (*init_routine)();
+                pthread_cleanup_pop(0);
+            } else {
+                (*init_routine)();
+            }
             os_mutex(once_control, MUTEX_WAKE_AND_SET, __PTHREAD_ONCE_IN_PROGRESS, __PTHREAD_ONCE_FINISHED, INT_MAX, NULL);
             break;
         }
