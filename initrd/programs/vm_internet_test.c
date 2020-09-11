5d055058b4244a125426cf4f7c72cc7ade5ba33b
ColeTrammer <cole.trammer@gmail.com>
2020-09-10 17:17:31 -0700

    vm_internet_test: add an option to test TCP

--- initrd/programs/vm_internet_test.c
+++ initrd/programs/vm_internet_test.c
@@ -7,44 +7,95 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-static void print_usage_and_exit(const char *s) {
-    fprintf(stderr, "Usage: %s [-r|-s] [-i ip-address] [-p port]\n", s);
-    exit(2);
-}
+static uint16_t port;
+static struct in_addr in_addr;
+static bool use_default_port = true;
+static bool use_default_addr = true;
+static bool do_send = false;
+static bool tcp = false;
 
-int main(int argc, char **argv) {
-    int opt;
-
-    uint16_t port;
-    struct in_addr in_addr;
-    bool use_default_port = true;
-    bool use_default_addr = true;
-    bool send = false;
-    while ((opt = getopt(argc, argv, ":i:p:rs")) != -1) {
-        switch (opt) {
-            case 'i':
-                if (inet_aton(optarg, &in_addr) == 0) {
-                    print_usage_and_exit(*argv);
-                }
-                use_default_addr = false;
-                break;
-            case 'p':
-                port = atoi(optarg);
-                use_default_port = false;
-                break;
-            case 'r':
-                send = false;
-                break;
-            case 's':
-                send = true;
-                break;
-            case ':':
-            case '?':
-                print_usage_and_exit(*argv);
-                break;
-        }
+static int do_tcp(void) {
+    if (use_default_port) {
+        port = 8823;
     }
 
+    int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (fd < 0) {
+        perror("vm_internet_test: socket");
+        return 1;
+    }
+
+    if (do_send) {
+        if (use_default_addr) {
+            in_addr.s_addr = htonl(INADDR_LOOPBACK);
+        }
+
+        struct sockaddr_in addr = {
+            .sin_family = AF_INET,
+            .sin_port = ntohs(port),
+            .sin_addr = in_addr,
+            .sin_zero = { 0 },
+        };
+
+        if (connect(fd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+            perror("vm_internet_test: connect");
+            return 1;
+        }
+
+        char buffer[4000] = "Hello, World";
+        if (send(fd, buffer, sizeof(buffer), 0) != sizeof(buffer)) {
+            perror("vm_internet_test: send");
+            return 1;
+        }
+
+        if (close(fd) < 0) {
+            perror("vm_internet_test: close");
+            return 1;
+        }
+
+        return 0;
+    }
+
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = ntohs(port),
+        .sin_addr = in_addr,
+        .sin_zero = { 0 },
+    };
+
+    if (bind(fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) {
+        perror("vm_internet_test: bind");
+        return 1;
+    }
+
+    if (listen(fd, 5) < 0) {
+        perror("vm_internet_test: listen");
+        return 1;
+    }
+
+    int sfd = accept(fd, NULL, NULL);
+    if (sfd < 0) {
+        perror("vm_internet_test: accept");
+        return 1;
+    }
+
+    char buffer[BUFSIZ];
+    ssize_t bytes;
+    if ((bytes = recv(sfd, buffer, sizeof(buffer), MSG_WAITALL)) < 0) {
+        perror("vm_internet_test: recvfrom");
+        return 1;
+    }
+
+    if (close(fd) < 0) {
+        perror("vm_internet_test: close");
+        return 1;
+    }
+
+    printf("[%ld]: '%s'\n", bytes, buffer);
+    return 0;
+}
+
+static int do_udp(void) {
     if (use_default_port) {
         port = 8888;
     }
@@ -55,7 +106,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    if (send) {
+    if (do_send) {
         if (use_default_addr) {
             in_addr.s_addr = htonl(INADDR_LOOPBACK);
         }
@@ -108,4 +159,46 @@ int main(int argc, char **argv) {
 
     printf("[%ld]: '%s'\n", bytes, buffer);
     return 0;
+}
+
+static void print_usage_and_exit(const char *s) {
+    fprintf(stderr, "Usage: %s [-r|-s] [-t] [-i ip-address] [-p port]\n", s);
+    exit(2);
+}
+
+int main(int argc, char **argv) {
+    int opt;
+    while ((opt = getopt(argc, argv, ":i:p:rst")) != -1) {
+        switch (opt) {
+            case 'i':
+                if (inet_aton(optarg, &in_addr) == 0) {
+                    print_usage_and_exit(*argv);
+                }
+                use_default_addr = false;
+                break;
+            case 'p':
+                port = atoi(optarg);
+                use_default_port = false;
+                break;
+            case 'r':
+                do_send = false;
+                break;
+            case 's':
+                do_send = true;
+                break;
+            case 't':
+                tcp = true;
+                break;
+            case ':':
+            case '?':
+                print_usage_and_exit(*argv);
+                break;
+        }
+    }
+
+    if (tcp) {
+        return do_tcp();
+    }
+
+    return do_udp();
 }
