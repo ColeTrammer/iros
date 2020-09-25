d374bd5a5b4d64379f00cb34db47dbab61fe45a7
ColeTrammer <cole.trammer@gmail.com>
2020-09-25 13:23:01 -0700

    libgraphics: fix Color::is()

--- libs/libgraphics/include/graphics/color.h
+++ libs/libgraphics/include/graphics/color.h
@@ -54,7 +54,7 @@ public:
 
     Maybe<vga_color> to_vga_color() const;
 
-    constexpr bool is(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0x00) const {
+    constexpr bool is(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xFF) const {
         return this->r() == r && this->g() == g && this->b() == b && this->a() == a;
     }
 
