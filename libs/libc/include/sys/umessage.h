5313ecf09e4ec41287554de0d5a1a82bb76fa71b
ColeTrammer <cole.trammer@gmail.com>
2020-09-29 16:25:01 -0700

    libc: use umessage interface in if_nameindex

--- libs/libc/include/sys/umessage.h
+++ libs/libc/include/sys/umessage.h
@@ -1,6 +1,7 @@
 #ifndef _SYS_UMESSAGE_H
 #define _SYS_UMESSAGE_H 1
 
+#include <net/if.h>
 #include <stdint.h>
 
 enum umessage_category_number {
@@ -14,5 +15,46 @@ struct umessage {
     uint16_t type;
     char data[0];
 };
+#define UMESSAGE_VALID(u, len) ((len) >= sizeof(struct umessage) && (u)->length == (len) && (u)->category < UMESSAGE_NUM_CATEGORIES)
+
+enum umessage_interface_request_type {
+    UMESSAGE_INTERFACE_LIST_REQUEST,
+    UMESSAGE_INTERFACE_NUM_REQUESTS,
+};
+#define UMESSAGE_INTERFACE_REQUEST_VALID(u, len) ((u)->category == UMESSAGE_INTERFACE && (u)->type < UMESSAGE_INTERFACE_NUM_REQUESTS)
+
+enum umessage_interface_message_type {
+    UMESSAGE_INTERFACE_LIST,
+    UMESSAGE_INTERFACE_NUM_MESSAGES,
+};
+#define UMESSAGE_INTERFACE_MESSAGE_VALID(u, len) \
+    (UMESSAGE_VALID(u, len) && (u)->category == UMESSAGE_INTERFACE && (u)->type < UMESSAGE_INTERFACE_NUM_MESSAGES)
+
+struct umessage_interface_list_request {
+    struct umessage base;
+};
+#define UMESSAFE_INTERFACE_LIST_REQUEST_VALID(u, len)                                                       \
+    (UMESSAGE_INTERFACE_REQUEST_VALID(u, len) && (len) >= sizeof(struct umessage_interface_list_request) && \
+     (u)->type == UMESSAGE_INTERFACE_LIST_REQUEST)
+
+struct umessage_interface_desc {
+    char name[IF_NAMESIZE];
+    int index;
+};
+
+struct umessage_interface_list {
+    struct umessage base;
+    size_t interface_count;
+    struct umessage_interface_desc interface_list[0];
+};
+#define UMESSAGE_INTERFACE_LIST_LENGTH(icount) (sizeof(struct umessage_interface_list) + (icount) * sizeof(struct umessage_interface_desc))
+#define UMESSAGE_INTERFACE_LIST_COUNT(length)            \
+    (((length) < sizeof(struct umessage_interface_list)) \
+         ? 0                                             \
+         : ((length) - sizeof(struct umessage_interface_list)) / sizeof(struct umessage_interface_desc))
+#define UMESSAGE_INTERFACE_LIST_VALID(u, len)                                                       \
+    (UMESSAGE_INTERFACE_MESSAGE_VALID(u, len) && (len) >= sizeof(struct umessage_interface_list) && \
+     (u)->type == UMESSAGE_INTERFACE_LIST &&                                                        \
+     ((struct umessage_interface_list*) (u))->interface_count == UMESSAGE_INTERFACE_LIST_COUNT(len))
 
 #endif /* _SYS_UMESSAGE_H */
--- libs/libc/net/if/if_nameindex.c
+++ libs/libc/net/if/if_nameindex.c
@@ -3,12 +3,21 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ioctl.h>
 #include <sys/param.h>
 #include <unistd.h>
 
+#ifdef __os_2__
+#include <sys/umessage.h>
+#else
+#include <sys/ioctl.h>
+#endif /* __os_2__ */
+
 struct if_nameindex *if_nameindex(void) {
+#ifdef __os_2__
+    int fd = socket(AF_UMESSAGE, SOCK_DGRAM, UMESSAGE_INTERFACE);
+#else
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
+#endif /* __os_2__ */
     if (fd < 0) {
         return NULL;
     }
@@ -18,8 +27,33 @@ struct if_nameindex *if_nameindex(void) {
     size_t max = 10;
     struct if_nameindex *nameindex = malloc(max * sizeof(struct if_nameindex));
 
+#ifdef __os_2__
+    struct umessage_interface_list_request req = {
+        .base = { .category = UMESSAGE_INTERFACE,
+                  .type = UMESSAGE_INTERFACE_LIST_REQUEST,
+                  .length = sizeof(struct umessage), 
+        },
+    };
+    if (write(fd, &req, req.base.length) < 0) {
+        goto error;
+    }
+
+    char buffer[2048];
+    ssize_t length;
+    if ((length = read(fd, buffer, sizeof(buffer))) < 0) {
+        goto error;
+    }
+
+    if (!UMESSAGE_INTERFACE_LIST_VALID((struct umessage *) buffer, (size_t) length)) {
+        goto error;
+    }
+    struct umessage_interface_list *list = (void *) buffer;
+    for (size_t i = 0; i < list->interface_count; i++) {
+        char *new_name = strdup(list->interface_list[i].name);
+        int new_index = list->interface_list[i].index;
+#else
     // NOTE: Probing the interface's by index sequentially won't work if for some reason the indexes aren't contiguous.
-    for (;;) {
+    for (;; i++) {
         struct ifreq req = { .ifr_ifindex = i + 1 };
         if (ioctl(fd, SIOCGIFNAME, &req) < 0) {
             if (errno != ENXIO) {
@@ -29,6 +63,9 @@ struct if_nameindex *if_nameindex(void) {
         }
 
         char *new_name = strdup(req.ifr_name);
+        int new_index = i + 1;
+#endif /* __os_2__ */
+
         if (!new_name) {
             goto error;
         }
@@ -42,11 +79,11 @@ struct if_nameindex *if_nameindex(void) {
         }
 
         nameindex[i].if_name = new_name;
-        nameindex[i].if_index = i + 1;
-        i++;
+        nameindex[i].if_index = new_index;
     }
 
     if (close(fd) < 0) {
+        fd = -1;
         goto error;
     }
 
@@ -56,11 +93,11 @@ struct if_nameindex *if_nameindex(void) {
     return nameindex;
 
 error:
-    nameindex[i].if_name = NULL;
-    nameindex[i].if_index = 0;
     if (nameindex) {
         if_freenameindex(nameindex);
     }
-    close(fd);
+    if (fd >= 0) {
+        close(fd);
+    }
     return NULL;
 }
