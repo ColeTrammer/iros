5a0a7825cb6bf9acc2d0c2563b6507c4f98ab00a
ColeTrammer <cole.trammer@gmail.com>
2020-10-16 15:35:02 -0700

    libipc: add libipc message declarations for the servers

--- userland/clipboard_server/include/clipboard_server/message.h
+++ userland/clipboard_server/include/clipboard_server/message.h
@@ -1,10 +1,32 @@
 #pragma once
 
+#include <ipc/gen.h>
 #include <liim/pointers.h>
 #include <liim/string.h>
 #include <stdlib.h>
 
 namespace ClipboardServer {
+
+// clang-format off
+IPC_MESSAGES(Client,
+    (GetContentsRequest,
+        (String, type),
+    ),
+    (SetContentsRequest,
+        (String, type),
+        (Vector<char>, data),
+    ),
+)
+
+IPC_MESSAGES(Server,
+    (GetContentsResponse,
+        (Vector<char>, data),
+    ),
+    (SetContentsResponse,
+        (bool, success),
+    ),
+)
+// clang-format on
 
 struct Message {
     enum class Type {
--- userland/window_server/include/window_server/message.h
+++ userland/window_server/include/window_server/message.h
@@ -13,10 +13,113 @@ typedef uint64_t wid_t;
 
 namespace WindowServer {
 
-enum class WindowType {
+enum WindowType {
     Application,
     Frameless,
 };
+
+}
+
+namespace LIIM {
+
+template<>
+struct Traits<scroll_state> {
+    static constexpr bool is_simple() { return true; }
+};
+
+template<>
+struct Traits<mouse_button_state> {
+    static constexpr bool is_simple() { return true; }
+};
+
+template<>
+struct Traits<key_event> {
+    static constexpr bool is_simple() { return true; }
+};
+
+template<>
+struct Traits<WindowServer::WindowType> {
+    static constexpr bool is_simple() { return true; }
+};
+
+}
+
+namespace WindowServer {
+
+// clang-format off
+IPC_MESSAGES(Client, 
+    (CreateWindowRequest,
+        (int, x),
+        (int, y),
+        (int, width),
+        (int, height),
+        (String, name),
+        (WindowType, type),
+        (wid_t, parent_id),
+    ),
+    (RemoveWindowRequest,
+        (wid_t, wid),
+    ),
+    (ChangeWindowVisibilityRequest,
+        (wid_t, wid),
+        (int, x),
+        (int, y),
+        (bool, visible),
+    ),
+    (SwapBufferRequest,
+        (wid_t, wid),
+    ),
+    (WindowReadyToResizeMessage,
+        (wid_t, wid),
+    ),
+    (WindowRenameRequest,
+        (wid_t, wid),
+        (String, name),
+    ),
+)
+
+IPC_MESSAGES(Server,
+    (CreateWindowResponse,
+        (wid_t, wid),
+        (size_t, size),
+        (String, path),
+    ),
+    (RemoveWindowResponse,
+        (bool, success),
+    ),
+    (ChangeWindowVisibilityResponse,
+        (wid_t, wid),
+        (bool, success),
+    ),
+    (KeyEventMessage,
+        (wid_t, wid),
+        (key_event, event),
+    ),
+    (MouseEventMessage,
+        (wid_t, wid),
+        (int, x),
+        (int, y),
+        (scroll_state, scroll),
+        (mouse_button_state, left),
+        (mouse_button_state, right),
+    ),
+    (WindowDidResizeMessage,
+        (wid_t, wid),
+    ),
+    (WindowReadyToResizeResponse,
+        (wid_t, wid),
+        (int, new_width),
+        (int, new_height),
+    ),
+    (WindowClosedEventMessage,
+        (wid_t, wid),
+    ),
+    (WindowStateChangeMessage,
+        (wid_t, wid),
+        (bool, active),
+    ),
+)
+// clang-format on
 
 struct Message {
     enum class Type {
@@ -303,5 +406,4 @@ struct Message {
         WindowStateChangeMessage window_state_change_message;
     } data;
 };
-
 };
