d537297383235665cab5ab726458659f64874ad2
ColeTrammer <cole.trammer@gmail.com>
2020-12-10 23:19:43 -0800

    theme_editor: ui skeleton for theme switching

--- userland/theme_editor/main.cpp
+++ userland/theme_editor/main.cpp
@@ -1,4 +1,6 @@
 #include <app/app.h>
+#include <app/box_layout.h>
+#include <app/button.h>
 #include <app/table_view.h>
 #include <app/window.h>
 
@@ -10,8 +12,15 @@ int main() {
     auto model = ThemeModel::create(nullptr);
 
     auto window = App::Window::create(nullptr, 100, 250, 400, 400, "Theme Editor");
-    auto& view = window->set_main_widget<App::TableView>();
+    auto& container = window->set_main_widget<App::Widget>();
+    auto& layout = container.set_layout<App::VerticalBoxLayout>();
+
+    auto& view = layout.add<App::TableView>();
     view.set_model(model);
+
+    auto& button = layout.add<App::Button>("Apply Theme");
+    button.set_preferred_size({ App::Size::Auto, 24 });
+    button.on_click = [&] {};
 
     app.enter();
     return 0;
