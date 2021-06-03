1cbcf0ec55b51b0c653bf95404b9585e605bcaaa
ColeTrammer <cole.trammer@gmail.com>
2021-06-02 21:37:17 -0700

    toolchain: add way to build cmake from source
    This allows the build system to use a version of CMake which thinks
    os_2 actually exists

---
+++ toolchain/cmake/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+ROOT="${ROOT:-$PWD/../..}"
+VERSION="3.20.3"
+DOWNLOAD_URL="https://github.com/Kitware/CMake/releases/download/v$VERSION/cmake-$VERSION.tar.gz"
+DOWNLOAD_DEST=cmake.tar.gz
+SRC="cmake-$VERSION"
+
+if [ ! -e $DOWNLOAD_DEST ]; then
+    curl -L "$DOWNLOAD_URL" -o "$DOWNLOAD_DEST"
+fi
+
+if [ ! -d "$SRC" ]; then
+    tar -xzf "$DOWNLOAD_DEST"
+    
+    cd "$SRC"
+    git init
+    git apply ../cmake.patch
+    cd ..
+fi
+
+if [ ! -e "$ROOT/toolchain/cross/bin/cmake" ]; then
+    cd "$SRC"
+    ./bootstrap --prefix="$ROOT/toolchain/cross" --parallel=5
+    make -j5
+    make install
+    cd ..
+fi
