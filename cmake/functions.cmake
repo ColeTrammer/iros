3b41e593d213dbf64cf7977f7a2a587b60495b46
ColeTrammer <cole.trammer@gmail.com>
2021-06-03 21:24:55 -0700

    build: simplify build mode detection by adding a toolchain file
    Now the build system does not rely on random enviornment variables.
    Instead, it native build requires no extra arguments and building
    the actual kernel simply requires passing the argument
    -DCMAKE_TOOLCHAIN_FILE=cmake/CMakeToolchain.txt.

---
+++ cmake/functions.cmake
@@ -0,0 +1,60 @@
+function(install_headers)
+    install(
+        DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/include/"
+        DESTINATION "usr/include"
+        FILES_MATCHING PATTERN "*.h"
+    )
+endfunction()
+
+function(add_all_targets)
+    foreach(TARGET ${TARGETS})
+        add_subdirectory(${TARGET})
+    endforeach()
+endfunction()
+
+function(add_os_headers name)
+    add_library(${name} INTERFACE)
+    target_include_directories(${name} INTERFACE include)
+    install_headers()
+endfunction()
+
+function(add_os_static_library target_name short_name has_headers)
+    add_library(${target_name} STATIC ${SOURCES})
+    install(TARGETS ${target_name} ARCHIVE DESTINATION usr/lib)
+    if (${has_headers})
+        target_include_directories(${target_name} PUBLIC include)
+        install_headers()
+    endif()
+    target_compile_definitions(${target_name} PRIVATE __is_static)
+    set_target_properties(${target_name} PROPERTIES OUTPUT_NAME ${short_name})
+endfunction()
+
+function(add_os_library target_name short_name has_headers)
+    add_library(${target_name} SHARED ${SOURCES})
+    install(TARGETS ${target_name} LIBRARY DESTINATION usr/lib)
+    if (${has_headers})
+        target_include_directories(${target_name} PUBLIC include)
+        install_headers()
+    endif()
+    target_compile_definitions(${target_name} PRIVATE __is_shared)
+    set_target_properties(${target_name} PROPERTIES OUTPUT_NAME ${short_name})
+endfunction()
+
+function(add_os_executable name dest_dir)
+    add_executable(${name} ${SOURCES})
+    if (NOT ${NATIVE_BUILD})
+        add_dependencies(${name} bootstrap-core-libs)
+        target_link_options(${name} PRIVATE "-Wl,-rpath-link" "${ROOT}/base/usr/lib") # Make the linker stop complaining about libgcc_s.so
+    endif()
+    install(TARGETS ${name} RUNTIME DESTINATION ${dest_dir})
+endfunction()
+
+function(add_os_static_executable name dest_dir)
+    add_executable(${name} ${SOURCES})
+    if (NOT ${NATIVE_BUILD})
+        add_dependencies(${name} bootstrap-core-libs)
+        target_link_libraries(${name} PRIVATE libc_static)
+    endif()
+    target_link_options(${name} PRIVATE "-static")
+    install(TARGETS ${name} RUNTIME DESTINATION ${dest_dir})
+endfunction()
--- setup.sh
+++ setup.sh
@@ -25,9 +25,6 @@ maybe_build_toolchain() {
     export BUILD_TOOLCHAIN='1'
 }
 
-export ARCH="$(uname -m)"
-export TARGET_OS="$(uname)"
-
 if exists 'ninja';
 then
     DEFAULT_GENERATOR='Ninja'
@@ -44,8 +41,6 @@ cmake -S . -B "$OS_2_NATIVE_DIR" -G "$GENERATOR" || die "Failed to do create nat
 
 cmake --build "$OS_2_NATIVE_DIR" || die "Failed to do native build"
 
-export TARGET_OS='os_2'
-export ARCH="${OS_ARCH:-x86_64}"
 export AS=${OS_AS:-"$ARCH-$TARGET_OS-as"}
 export CC=${OS_CC:-"$ARCH-$TARGET_OS-gcc"}
 export CXX=${OS_CXX:-"$ARCH-$TARGET_OS-g++"}
@@ -54,11 +49,10 @@ exists "$AS" || maybe_build_toolchain || die "$AS does not exist"
 exists "$CC" || die "$CC does not exist" 
 exists "$CXX" || die "$CXX does not exist" 
 
-OS_2_BUILD_DIR="${OS_2_BUILD_DIR:-build}"
-OS_2_BUILD_DIR="$(realpath $OS_2_BUILD_DIR)"
 cmake -S . -B "$OS_2_BUILD_DIR" -G "$GENERATOR" \
     -DCMAKE_C_COMPILER_WORKS=TRUE \
     -DCMAKE_CXX_COMPILER_WORKS=TRUE \
+    -DCMAKE_TOOLCHAIN_FILE=cmake/CMakeToolchain.txt \
     || die "Failed to create os cmake project at" "$OS_2_BUILD_DIR"
 
 if [ "$BUILD_TOOLCHAIN" ];
