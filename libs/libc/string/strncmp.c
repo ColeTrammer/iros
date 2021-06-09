c1e302db14f808689b78c1bddd4f3ddb5905c3a9
ColeTrammer <cole.trammer@gmail.com>
2021-06-09 13:42:41 -0700

    libc: fix strange bug in strncmp()
    Strings are terminated by '\0', not '\n'.

--- libs/libc/string/strncmp.c
+++ libs/libc/string/strncmp.c
@@ -3,7 +3,7 @@
 
 int strncmp(const char *s1, const char *s2, size_t n) {
     size_t i = 0;
-    for (; i < n && s1[i] != '\0' && s2[i] != '\n'; i++) {
+    for (; i < n && s1[i] != '\0' && s2[i] != '\0'; i++) {
         if (s1[i] < s2[i]) {
             return -1;
         } else if (s1[i] > s2[i]) {
