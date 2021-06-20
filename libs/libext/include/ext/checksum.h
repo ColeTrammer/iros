7623987175312c2fae6c41f7a0b62621e43be16b
ColeTrammer <cole.trammer@gmail.com>
2021-06-20 15:23:41 -0700

    libext: add checksum functions for the ADLER32 algorithm

--- libs/libext/include/ext/checksum.h
+++ libs/libext/include/ext/checksum.h
@@ -8,8 +8,15 @@
 extern "C" {
 #endif /* __cplusplus */
 
+#define CHECKSUM_CRC32_INIT    ((uint32_t) 0)
+#define CHECKSUM_ADLER32_INIT  ((uint32_t) 1)
+#define CHECKSUM_INTERNET_INIT ((uint16_t)(~0U & 0xFFFFU))
+
 uint32_t compute_partial_crc32_checksum(const void *data, size_t num_bytes, uint32_t crc);
 uint32_t compute_crc32_checksum(const void *data, size_t num_bytes);
+
+uint32_t compute_partial_adler32_checksum(const void *data, size_t num_bytes, uint32_t adler);
+uint32_t compute_adler32_checksum(const void *data, size_t num_bytes);
 
 uint16_t compute_partial_internet_checksum(const void *packet, size_t num_bytes, uint16_t start);
 uint16_t compute_internet_checksum(const void *data, size_t bytes);
