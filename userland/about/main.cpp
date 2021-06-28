3c09193704aa9b5bac374d4cf2d5d8044a40bdff
ColeTrammer <cole.trammer@gmail.com>
2021-06-27 22:38:52 -0700

    about: add test menubar

--- userland/about/main.cpp
+++ userland/about/main.cpp
@@ -2,6 +2,7 @@
 #include <app/box_layout.h>
 #include <app/button.h>
 #include <app/context_menu.h>
+#include <app/menubar.h>
 #include <app/text_label.h>
 #include <app/widget.h>
 #include <app/window.h>
@@ -27,11 +28,28 @@ int main() {
     auto window = App::Window::create(nullptr, 300, 300, 250, 250, "About");
     auto& main_widget = window->set_main_widget<App::Widget>();
 
-    auto& layout = main_widget.set_layout<App::BoxLayout>(App::BoxLayout::Orientation::Vertical);
-    auto& label = layout.add<App::TextLabel>("Hello World!");
-    layout.add<App::Widget>();
-    layout.add<TestWidget>();
-    auto& button = layout.add<App::Button>("Click Me!");
+    auto& root_layout = main_widget.set_layout<App::BoxLayout>(App::BoxLayout::Orientation::Vertical);
+    root_layout.set_margins({ 0, 0, 0, 0 });
+    root_layout.set_spacing(0);
+
+    auto& menubar = root_layout.add<App::Menubar>();
+
+    menubar.create_menu("File");
+
+    auto& edit_menu = menubar.create_menu("Edit");
+    edit_menu.add_menu_item("Edit", [] {
+        printf("Editing\n");
+    });
+
+    auto& help_menu = menubar.create_menu("Help");
+    help_menu.add_menu_item("Help", [] {
+        printf("Helping\n");
+    });
+
+    auto& label = root_layout.add<App::TextLabel>("Hello World!");
+    root_layout.add<App::Widget>();
+    root_layout.add<TestWidget>();
+    auto& button = root_layout.add<App::Button>("Click Me!");
     button.on_click = [] {
         printf("clicked!\n");
     };
