bbceb75b263a4bc91016cf24dde9845495b35300
ColeTrammer <cole.trammer@gmail.com>
2021-07-02 10:06:20 -0700

    libapp: flesh out av event loop for SDL
    This runs the SDL event loop on a separate thread, which will then
    translate the SDL events into regular App::Events.

--- libs/libapp/application_sdl.cpp
+++ libs/libapp/application_sdl.cpp
@@ -1,9 +1,50 @@
 #include <app/application_sdl.h>
+#include <unistd.h>
+
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
 
 namespace App {
+static void* thread_run_sdl(void*) {
+    SDLApplication::the().run_sdl();
+    return nullptr;
+}
+
+SDLApplication& SDLApplication::the() {
+    auto& app = Application::the();
+    assert(app.is_sdl_application());
+    return static_cast<SDLApplication&>(app);
+}
+
 SDLApplication::SDLApplication() {
     initialize_palette(Palette::create_default());
+
+    pthread_barrier_init(&m_sdl_init_barrier, nullptr, 2);
+    pthread_create(&m_sdl_thread, nullptr, thread_run_sdl, nullptr);
+    wait_for_sdl_init();
 }
 
 SDLApplication::~SDLApplication() {}
+
+void SDLApplication::run_sdl() {
+    SDL_SetMainReady();
+    assert(SDL_Init(SDL_INIT_VIDEO) == 0);
+
+    pthread_barrier_wait(&m_sdl_init_barrier);
+
+    for (;;) {
+        SDL_Event event;
+        SDL_WaitEvent(&event);
+
+        switch (event.type) {
+            default:
+                break;
+        }
+    }
+}
+
+void SDLApplication::wait_for_sdl_init() {
+    pthread_barrier_wait(&m_sdl_init_barrier);
+}
 }
--- libs/libapp/include/app/application_sdl.h
+++ libs/libapp/include/app/application_sdl.h
@@ -1,14 +1,22 @@
 #pragma once
 
 #include <app/application.h>
+#include <pthread.h>
 
 namespace App {
 class SDLApplication final : public Application {
 public:
+    static SDLApplication& the();
     SDLApplication();
     virtual ~SDLApplication() override;
 
+    void run_sdl();
+    void wait_for_sdl_init();
+
 private:
     virtual bool is_sdl_application() const { return true; }
+
+    pthread_t m_sdl_thread;
+    pthread_barrier_t m_sdl_init_barrier;
 };
 }
