2d2eb3f45f09bb599fc9489d05a61b539217dc15
ColeTrammer <cole.trammer@gmail.com>
2021-07-13 21:50:54 -0700

    libedit: really add LineRenderer class

---
+++ libs/libedit/include/edit/line_renderer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <edit/line.h>
+#include <liim/forward.h>
+#include <liim/string.h>
+
+namespace Edit {
+class LineRenderer {
+public:
+    LineRenderer(int max_width, bool word_wrap_enabled) : m_max_width(max_width), m_word_wrap_enabled(word_wrap_enabled) {}
+
+    RenderedLine finish();
+
+    void begin_segment(int index_into_line, int optional_metadata, PositionRangeType type);
+    void add_to_segment(const StringView& text, int display_width);
+    void end_segment();
+
+    int absolute_col_position() const { return m_absolute_col_position; }
+    const Position& current_position() const { return m_current_position; }
+
+private:
+    RenderedLine m_rendered_line;
+    String m_current_rendered_line;
+    PositionRange m_current_range;
+    Position m_current_position;
+    int m_absolute_col_position { 0 };
+    int m_max_width { 0 };
+    bool m_word_wrap_enabled { false };
+};
+}
---
+++ libs/libedit/line_renderer.cpp
@@ -0,0 +1,38 @@
+#include <edit/line_renderer.h>
+
+namespace Edit {
+RenderedLine LineRenderer::finish() {
+    if (m_rendered_line.rendered_lines.empty() || !m_current_rendered_line.is_empty()) {
+        m_rendered_line.rendered_lines.add(move(m_current_rendered_line));
+    }
+    return move(m_rendered_line);
+}
+
+void LineRenderer::begin_segment(int index_into_line, int optional_metadata, PositionRangeType type) {
+    m_current_range.start = current_position();
+    m_current_range.optional_metadata = optional_metadata;
+    m_current_range.index_into_line = index_into_line;
+    m_current_range.type = type;
+}
+
+void LineRenderer::add_to_segment(const StringView& text, int display_width) {
+    if (display_width > m_max_width) {
+        display_width = m_max_width;
+    }
+
+    if (m_word_wrap_enabled && current_position().col + display_width > m_max_width) {
+        m_current_position.row++;
+        m_current_position.col = 0;
+        m_rendered_line.rendered_lines.add(move(m_current_rendered_line));
+    }
+
+    m_current_rendered_line += String(text);
+    m_current_position.col += display_width;
+    m_absolute_col_position += display_width;
+}
+
+void LineRenderer::end_segment() {
+    m_current_range.end = current_position();
+    m_rendered_line.position_ranges.add(move(m_current_range));
+}
+}
