d591d18f2f8737db7785d6792b00abc8bab97967
ColeTrammer <cole.trammer@gmail.com>
2021-07-24 14:55:44 -0700

    tests: convert the initrd alarm test program to use the new framework

---
+++ initrd/programs/test_argv.c
@@ -0,0 +1,85 @@
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    (void) argc;
+    (void) argv;
+
+    /* Test execve */
+
+    // pid_t f = fork();
+    // if (f == 0) {
+    //     execvp("ls", argv);
+
+    //     perror("Test");
+
+    //     /* Should Not Execute */
+    //     return 1;
+    // } else if (f < 0) {
+    //     perror("Test");
+    //     return 1;
+    // } else {
+    //     int status;
+
+    //     do {
+    //          waitpid(f, &status, WUNTRACED);
+    //     } while (!WIFEXITED(status));
+    // }
+
+    /* Test args */
+
+    printf("Argc: %d\n", argc);
+    for (size_t i = 0; argv[i] != NULL; i++) {
+        printf("Argv[%ld]: %s\n", i, argv[i]);
+    }
+
+    // char **e = environ;
+    // while (*e != NULL) {
+    //     puts(*e);
+    //     e++;
+    // }
+
+    /* Test Writing To stdio */
+
+    // const char *test_str = "Testing STDIO";
+    // puts(test_str);
+
+    /* Test Reading Files Using stdio.h */
+
+    // FILE *a = fopen("/a.txt", "r");
+    // char *buffer = malloc(0x5000);
+    // fread(buffer, 0x5000, 1, a);
+    // puts(buffer);
+    // fclose(a);
+
+    /* Test Reading Files Using Sys Calls */
+
+    // int fd = open("/a.txt", 0, 0);
+    // char *buffer = malloc(0x5000);
+    // read(fd, buffer, 0x5000);
+    // puts(buffer);
+    // close(fd);
+
+    /* Test Fork Sys Call */
+
+    // pid_t ret = fork();
+    // if (ret == 0) {
+    //     for (int i = 0; i <= 2; i++) {
+    //         printf("%d: Child\n", i);
+    //     }
+    //     return 0;
+    // }
+
+    // for (int i = 0; i <= 3; i++) {
+    //     printf("%d: Parent\n", i);
+    // }
+
+    return 0;
+}
---
+++ tests/kernel/userland/test_alarm.cpp
@@ -0,0 +1,65 @@
+#include <errno.h>
+#include <signal.h>
+#include <test/test.h>
+#include <unistd.h>
+
+#ifndef BINARY_DIR
+#define BINARY_DIR "."
+#endif
+
+TEST(alarm, basic) {
+    static sig_atomic_t did_get_alarm = 0;
+    signal(SIGALRM, [](int) {
+        did_get_alarm = 1;
+    });
+
+    sigset_t set;
+    sigfillset(&set);
+    sigdelset(&set, SIGALRM);
+    sigdelset(&set, SIGINT);
+
+    EXPECT(!did_get_alarm);
+    alarm(1);
+    EXPECT(sigsuspend(&set) == -1);
+    EXPECT(errno == EINTR);
+    EXPECT(did_get_alarm);
+}
+
+TEST(alarm, cancel) {
+    static sig_atomic_t did_get_alarm = 0;
+    signal(SIGALRM, [](int) {
+        did_get_alarm = 1;
+    });
+
+    EXPECT(!did_get_alarm);
+    alarm(1);
+    alarm(0);
+    usleep(1000500);
+    EXPECT(!did_get_alarm);
+}
+
+TEST(alarm, exec) {
+    auto exit_status = Test::TestManager::the().spawn(
+        [] {
+            alarm(1);
+        },
+        BINARY_DIR "/test_alarm_exec_helper");
+    EXPECT(exit_status == 0);
+}
+
+#ifdef ALARM_EXEC_HELPER
+int main() {
+    static sig_atomic_t did_get_alarm = 0;
+    signal(SIGALRM, [](int) {
+        did_get_alarm = 1;
+    });
+
+    sigset_t set;
+    sigfillset(&set);
+    sigdelset(&set, SIGALRM);
+    sigdelset(&set, SIGINT);
+
+    sigsuspend(&set);
+    return did_get_alarm ? 0 : 1;
+}
+#endif
