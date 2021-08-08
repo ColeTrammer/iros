9c213cd2d5e22a6b7966d0679c9957e19f4c94dc
ColeTrammer <cole.trammer@gmail.com>
2021-08-07 22:05:21 -0700

    libliim: add format logging functions (debug_log, error_log, out_log)
    Gone are the days of fprintf(stderr, "%s", string.string()).
    Now just do error_log("{}", string).
    
    Note that debug_log goes straight to stderr but could be redirected
    to a file instead.

--- libs/libliim/include/liim/format.h
+++ libs/libliim/include/liim/format.h
@@ -1,6 +1,31 @@
 #pragma once
 
 #include <liim/format/format.h>
+#include <stdio.h>
 
+namespace LIIM {
+template<typename... Args>
+void debug_log(StringView format_string, const Args&... args) {
+    auto result = vformat(format_string, Format::make_format_args(args...));
+    fprintf(stderr, "%s\n", result.string());
+}
+
+template<typename... Args>
+void error_log(StringView format_string, const Args&... args) {
+    auto result = vformat(format_string, Format::make_format_args(args...));
+    fprintf(stderr, "%s\n", result.string());
+}
+
+template<typename... Args>
+void out_log(StringView format_string, const Args&... args) {
+    auto result = vformat(format_string, Format::make_format_args(args...));
+    puts(result.string());
+}
+}
+
+using LIIM::debug_log;
+using LIIM::error_log;
+using LIIM::out_log;
 using LIIM::Format::format;
+using LIIM::Format::make_format_args;
 using LIIM::Format::vformat;
