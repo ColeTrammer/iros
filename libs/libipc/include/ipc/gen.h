217e2af6589cb28ca0876852a8cda06e2f9f9356
ColeTrammer <cole.trammer@gmail.com>
2021-08-20 10:51:53 -0700

    libipc: use new liim/preprocessor.h to implement IPC generators

--- libs/libipc/include/ipc/gen.h
+++ libs/libipc/include/ipc/gen.h
@@ -4,76 +4,16 @@
 #include <ipc/message.h>
 #include <ipc/message_dispatcher.h>
 #include <ipc/stream.h>
-
-#define __PRIMITIVE_CAT(a, ...) a##__VA_ARGS__
-
-#define __IFF(c)        __PRIMITIVE_CAT(__IFF_, c)
-#define __IFF_0(t, ...) __VA_ARGS__
-#define __IFF_1(t, ...) t
-
-#define __COMPL(b) __PRIMITIVE_CAT(__COMPL_, b)
-#define __COMPL_0  1
-#define __COMPL_1  0
-
-#define __CHECK_N(x, n, ...) n
-#define __CHECK(...)         __CHECK_N(__VA_ARGS__, 0, )
-#define __PROBE(x)           x, 1,
-
-#define __IS_PAREN_PROBE(...) __PROBE(~)
-#define __IS_PAREN(x)         __CHECK(__IS_PAREN_PROBE x)
-
-#define __NOT(x) __CHECK(__PRIMITIVE_CAT(__NOT_, x))
-#define __NOT_0  __PROBE(~)
-
-#define __BOOL(x) __COMPL(__NOT(x))
-#define __IF(c)   __IFF(__BOOL(c))
-
-#define __EMPTY(...)
-#define __DEFER(...)    __VA_ARGS__ __EMPTY()
-#define __OBSTRUCT(...) __VA_ARGS__ __DEFER(__EMPTY)()
-
-#define __EVAL(...)  __EVAL1(__EVAL1(__EVAL1(__VA_ARGS__)))
-#define __EVAL1(...) __EVAL2(__EVAL2(__EVAL2(__VA_ARGS__)))
-#define __EVAL2(...) __EVAL3(__EVAL3(__EVAL3(__VA_ARGS__)))
-#define __EVAL3(...) __EVAL4(__EVAL4(__EVAL4(__VA_ARGS__)))
-#define __EVAL4(...) __EVAL5(__EVAL5(__EVAL5(__VA_ARGS__)))
-#define __EVAL5(...) __VA_ARGS__
-
-#define __EVAL_(...)  __EVAL1_(__EVAL1_(__EVAL1_(__VA_ARGS__)))
-#define __EVAL1_(...) __EVAL2_(__EVAL2_(__EVAL2_(__VA_ARGS__)))
-#define __EVAL2_(...) __EVAL3_(__EVAL3_(__EVAL3_(__VA_ARGS__)))
-#define __EVAL3_(...) __EVAL4_(__EVAL4_(__EVAL4_(__VA_ARGS__)))
-#define __EVAL4_(...) __EVAL5_(__EVAL5_(__EVAL5_(__VA_ARGS__)))
-#define __EVAL5_(...) __VA_ARGS__
-
-#define __EVAL__(...)  __EVAL1__(__EVAL1__(__EVAL1__(__VA_ARGS__)))
-#define __EVAL1__(...) __EVAL2__(__EVAL2__(__EVAL2__(__VA_ARGS__)))
-#define __EVAL2__(...) __EVAL3__(__EVAL3__(__EVAL3__(__VA_ARGS__)))
-#define __EVAL3__(...) __EVAL4__(__EVAL4__(__EVAL4__(__VA_ARGS__)))
-#define __EVAL4__(...) __EVAL5__(__EVAL5__(__EVAL5__(__VA_ARGS__)))
-#define __EVAL5__(...) __VA_ARGS__
-
-#define __FIRST(x, ...) x
-#define __NEXT(x, ...)  __VA_ARGS__
-
-#define __RECURSIVE_ITER_INDIRECT() __RECURSIVE_ITER
-#define __RECURSIVE_ITER(op, ...)          \
-    __IF(__IS_PAREN(__FIRST(__VA_ARGS__))) \
-    (op __FIRST(__VA_ARGS__) __OBSTRUCT(__RECURSIVE_ITER_INDIRECT)()(op, __NEXT(__VA_ARGS__)), __VA_ARGS__)
-
-#define __RECURSIVE_ITER__INDIRECT() __RECURSIVE_ITER_
-#define __RECURSIVE_ITER_(op, ...)         \
-    __IF(__IS_PAREN(__FIRST(__VA_ARGS__))) \
-    (__EVAL_(op __FIRST(__VA_ARGS__)) __OBSTRUCT(__RECURSIVE_ITER__INDIRECT)()(op, __NEXT(__VA_ARGS__)), __VA_ARGS__)
+#include <liim/preprocessor.h>
 
 #define __MESSAGE_FIELD_DECL(type, name)               type name;
 #define __MESSAGE_FIELD_SERIALIZATION_SIZE(type, name) ret += IPC::Serializer<type>::serialization_size(this->name);
 #define __MESSAGE_FIELD_SERIALIZER(type, name)         stream << this->name;
 #define __MESSAGE_FIELD_DESERIALIZER(type, name)       stream >> this->name;
 
-#define __MESSAGE_DECL(...)                    __EVAL(__RECURSIVE_ITER(__MESSAGE_FIELD_DECL, ##__VA_ARGS__))
-#define __MESSAGE_SERIALIZATION_SIZE_BODY(...) __EVAL(__RECURSIVE_ITER(__MESSAGE_FIELD_SERIALIZATION_SIZE, ##__VA_ARGS__))
-#define __MESSAGE_SERIALIZER_BODY(...)         __EVAL(__RECURSIVE_ITER(__MESSAGE_FIELD_SERIALIZER, ##__VA_ARGS__))
+#define __MESSAGE_DECL(...)                    LIIM_FOR_EACH(__MESSAGE_FIELD_DECL, ##__VA_ARGS__)
+#define __MESSAGE_SERIALIZATION_SIZE_BODY(...) LIIM_FOR_EACH(__MESSAGE_FIELD_SERIALIZATION_SIZE, ##__VA_ARGS__)
+#define __MESSAGE_SERIALIZER_BODY(...)         LIIM_FOR_EACH(__MESSAGE_FIELD_SERIALIZER, ##__VA_ARGS__)
 #define __MESSAGE_SERIALIZER(n, ...)                   \
     uint32_t serialization_size() const {              \
         uint32_t ret = 2 * sizeof(uint32_t);           \
@@ -87,7 +27,7 @@
         return !stream.error();                        \
     }
 
-#define __MESSAGE_DESERIALIZER_BODY(...) __EVAL(__RECURSIVE_ITER(__MESSAGE_FIELD_DESERIALIZER, ##__VA_ARGS__))
+#define __MESSAGE_DESERIALIZER_BODY(...) LIIM_FOR_EACH(__MESSAGE_FIELD_DESERIALIZER, ##__VA_ARGS__)
 #define __MESSAGE_DESERIALIZER(n, ...)                \
     bool deserialize(IPC::Stream& stream) {           \
         uint32_t size;                                \
@@ -131,15 +71,15 @@
         break;                          \
     }
 
-#define __IPC_MESSAGE_TYPES(...)                                                 \
-    enum class Type : uint32_t {                                                 \
-        __EVAL__(__RECURSIVE_ITER_(__MESSAGE_TYPE, ##__VA_ARGS__)) MessageCount, \
+#define __IPC_MESSAGE_TYPES(...)                                   \
+    enum class Type : uint32_t {                                   \
+        LIIM_FOR_EACH(__MESSAGE_TYPE, ##__VA_ARGS__) MessageCount, \
     };
-#define __IPC_MESSAGES(...) __EVAL__(__RECURSIVE_ITER_(__MESSAGE, ##__VA_ARGS__))
+#define __IPC_MESSAGES(...) LIIM_FOR_EACH(__MESSAGE, ##__VA_ARGS__)
 #define __IPC_DISPATCHER(...)                                                                   \
     class MessageDispatcher : public IPC::MessageDispatcher {                                   \
     public:                                                                                     \
-        __EVAL__(__RECURSIVE_ITER_(__DISPATCHER_DECL, ##__VA_ARGS__))                           \
+        LIIM_FOR_EACH(__DISPATCHER_DECL, ##__VA_ARGS__)                                         \
         virtual void handle_incoming_data(IPC::Endpoint& endpoint, IPC::Stream& stream) final { \
             uint32_t size;                                                                      \
             uint32_t type;                                                                      \
@@ -155,7 +95,7 @@
             }                                                                                   \
             stream.rewind();                                                                    \
             switch (static_cast<Type>(type)) {                                                  \
-                __EVAL__(__RECURSIVE_ITER_(__DISPATCHER, ##__VA_ARGS__))                        \
+                LIIM_FOR_EACH(__DISPATCHER, ##__VA_ARGS__)                                      \
                 default:                                                                        \
                     handle_error(endpoint);                                                     \
                     return;                                                                     \
@@ -163,9 +103,9 @@
         }                                                                                       \
     };
 
-#define IPC_MESSAGES(n, ...)         \
-    namespace n {                    \
-    __IPC_MESSAGE_TYPES(__VA_ARGS__) \
-    __IPC_MESSAGES(__VA_ARGS__)      \
-    __IPC_DISPATCHER(__VA_ARGS__)    \
+#define IPC_MESSAGES(n, ...)                    \
+    namespace n {                               \
+    LIIM_EVAL(__IPC_MESSAGE_TYPES(__VA_ARGS__)) \
+    LIIM_EVAL(__IPC_MESSAGES(__VA_ARGS__))      \
+    LIIM_EVAL(__IPC_DISPATCHER(__VA_ARGS__))    \
     }
