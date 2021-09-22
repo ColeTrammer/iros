6812196b0f42f223c2a550421d9201b7f9b59dd2
ColeTrammer <cole.trammer@gmail.com>
2021-09-21 18:12:05 -0700

    libtinput: naively handle unicode mark characters
    When encoutering a unicode mark charcter, we append it to the previous
    glyph within incrementing the text's width. This allows the text editor
    to properly handle nonsensical text like:
    
    h̷͙͔̫̭̔̋̐͛͜e̷͇͓͉̜̜̿͂͛̍͝l̶̠̹̣͙̫͑̋͑l̶̢̪̜̆͜o̷͇̯͍̟̓̐͝
    
    and treat every long sequence of code points as a single terminal
    cell.

--- libs/libtinput/include/tinput/terminal_glyph.h
+++ libs/libtinput/include/tinput/terminal_glyph.h
@@ -8,6 +8,7 @@ class TerminalGlyph {
 public:
     TerminalGlyph(String text, int width) : m_text(move(text)), m_width(width) {}
 
+    String& text() { return m_text; }
     const String& text() const { return m_text; }
     int width() const { return m_width; }
 
@@ -26,8 +27,12 @@ public:
     auto begin() const { return m_glyphs.begin(); }
     auto end() const { return m_glyphs.end(); }
 
+    TerminalGlyph& first() { return m_glyphs.first(); }
+    TerminalGlyph& last() { return m_glyphs.last(); }
+
     int total_width() const { return m_total_width; }
     int size() const { return m_glyphs.size(); }
+    bool empty() const { return m_glyphs.empty(); }
 
     const TerminalGlyph& operator[](int index) const { return m_glyphs[index]; }
 
