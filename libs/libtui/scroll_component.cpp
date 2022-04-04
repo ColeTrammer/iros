474d2653e4fa6480db7c8900d5be30f5bd5d726e
ColeTrammer <cole.trammer@gmail.com>
2022-04-03 23:41:23 -0700

    libtui: implement TUI::ScrollComponent::draw_scrollbars()
    This implementation is pretty much a copy paste from the GUI version,
    but still works. However, it would be nice if the scrollbar's length
    decreased as the scrollable area increased (like you would expect).

--- libs/libtui/scroll_component.cpp
+++ libs/libtui/scroll_component.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <tinput/terminal_renderer.h>
 #include <tui/panel.h>
 #include <tui/scroll_component.h>
@@ -16,7 +17,39 @@ TInput::TerminalRenderer ScrollComponent::get_renderer() {
     return renderer;
 }
 
-void ScrollComponent::draw_scrollbars() {}
+void ScrollComponent::draw_scrollbars() {
+    auto renderer = panel().get_renderer();
+    auto scrollbar_width = this->scrollbar_width();
+
+    if (draw_horizontal_scrollbar()) {
+        renderer.clear_rect({ 0, panel().sized_rect().height() - scrollbar_width, panel().sized_rect().width(), scrollbar_width });
+
+        double percent_scrolled_x = scroll_offset().x() / static_cast<double>(total_rect().width() - available_rect().width());
+
+        int fixed_width = 5;
+        int bar_offset = ceil(panel().sized_rect().width() - fixed_width) * percent_scrolled_x;
+
+        renderer.clear_rect({ bar_offset, panel().sized_rect().height() - scrollbar_width, fixed_width, scrollbar_width },
+                            { ColorValue::White });
+    }
+
+    if (draw_vertical_scrollbar()) {
+        renderer.clear_rect({ panel().sized_rect().width() - scrollbar_width, 0, scrollbar_width, panel().sized_rect().height() });
+
+        double percent_scrolled_y = scroll_offset().y() / static_cast<double>(total_rect().height() - available_rect().height());
+
+        int fixed_height = 5;
+        int bar_offset = ceil(panel().sized_rect().height() - fixed_height) * percent_scrolled_y;
+        renderer.clear_rect(
+            {
+                panel().sized_rect().width() - scrollbar_width,
+                bar_offset,
+                scrollbar_width,
+                fixed_height,
+            },
+            { ColorValue::White });
+    }
+}
 
 void ScrollComponent::attach_to_base(App::ScrollComponent& base) {
     m_base = &base;
