47aa977edbb3d4815f454b9ed3d266f5f0984d2f
ColeTrammer <cole.trammer@gmail.com>
2022-05-10 01:27:46 +0000

    libext: add forward declaration header

---
+++ libs/libext/include/ext/forward.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Ext {
+class DeflateDecoder;
+class DeflateEncoder;
+class GZipDecoder;
+class GZipEncoder;
+class File;
+class MappedFile;
+class Mode;
+class Path;
+class StreamDecoder;
+class StreamEncoder;
+class Stream;
+class ZLibStreamDecoder;
+}
