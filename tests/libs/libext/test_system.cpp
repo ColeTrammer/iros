dbdeaaeb7edd289ab29e27eecdcb5c5371f8582a
ColeTrammer <cole.trammer@gmail.com>
2022-05-21 00:42:06 +0000

    tests: explicitly mount procfs using before /proc/self/exe on iros

--- tests/libs/libext/test_system.cpp
+++ tests/libs/libext/test_system.cpp
@@ -1,8 +1,16 @@
 #include <ext/path.h>
 #include <ext/system.h>
+#include <sys/mount.h>
 #include <test/test.h>
+#include <unistd.h>
 
 TEST(system, realpath) {
+#ifdef __iros__
+    if (getpid() == 1) {
+        EXPECT_EQ(mount("", "/proc", "procfs", 0, nullptr), 0);
+    }
+#endif
+
     auto result = Ext::realpath("/proc/self/exed");
     EXPECT(result.is_error());
     EXPECT_EQ(result.error().system_call(), "realpath");
