61c206566c32fafdf808f805230afdeeb0fd8494
ColeTrammer <cole.trammer@gmail.com>
2022-07-12 01:53:24 +0000

    libc: use liim's sort() to implement qsort()
    This is a massive improvement over the old algorithm, which
    allocated memory and used selection sort instead of
    a N*log(N) algorithm.

---
+++ libs/libc/stdlib/qsort.cpp
@@ -0,0 +1,36 @@
+#include <liim/container/algorithm/sort.h>
+#include <liim/container/iterator/continuous_iterator.h>
+#include <liim/container/producer/range.h>
+
+class CIterator : public ContinuousIteratorAdapter<CIterator> {
+public:
+    CIterator(void* base, size_t index, size_t size) : ContinuousIteratorAdapter(index), m_base(base), m_size(size) {}
+
+    using ValueType = void*;
+
+    ValueType operator*() { return static_cast<void*>(static_cast<char*>(m_base) + this->index() * m_size); }
+
+    void swap_contents(CIterator& other) {
+        uint8_t* a = static_cast<uint8_t*>(**this);
+        uint8_t* b = static_cast<uint8_t*>(*other);
+        for (auto i : range(m_size)) {
+            a[i] ^= b[i];
+            b[i] ^= a[i];
+            a[i] ^= b[i];
+        }
+    }
+
+private:
+    void* m_base { nullptr };
+    size_t m_size { 0 };
+};
+
+extern "C" void qsort(void* base, size_t nmemb, size_t size, int (*compar)(const void* a, const void* b)) {
+    if (base == nullptr || nmemb == 0 || size == 0 || compar == nullptr) {
+        return;
+    }
+
+    sort(iterator_container(CIterator(base, 0, size), CIterator(base, nmemb, size)), [&](auto* a, auto* b) {
+        return compar(a, b) <=> 0;
+    });
+}
--- libs/libc/stdlib/stdlib.c
+++ libs/libc/stdlib/stdlib.c
@@ -59,47 +59,6 @@ lldiv_t lldiv(long long a, long long b) {
     return (lldiv_t) { a / b, a % b };
 }
 
-#define QSORT_AT(p, in, size) ((void *) (((uintptr_t)(p)) + (in) * (size)))
-
-// Use selection sort for simplicity
-void qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *a, const void *b)) {
-    if (base == NULL || nmemb == 0 || size == 0 || compar == NULL) {
-        return;
-    }
-
-    void *temp;
-    if (size > 0x200) {
-        temp = malloc(size);
-    } else {
-        temp = alloca(size);
-    }
-
-    for (size_t i = 0; i < nmemb - 1; i++) {
-        void *to_replace = QSORT_AT(base, i, size);
-        void *min = to_replace;
-
-        for (size_t j = i + 1; j < nmemb; j++) {
-            void *curr = QSORT_AT(base, j, size);
-            if (compar(curr, min) < 0) {
-                min = curr;
-            }
-        }
-
-        if (to_replace == min) {
-            continue;
-        }
-
-        // Do swap
-        memcpy(temp, to_replace, size);
-        memcpy(to_replace, min, size);
-        memcpy(min, temp, size);
-    }
-
-    if (size > 0x200) {
-        free(temp);
-    }
-}
-
 #ifndef __is_libk
 int system(const char *cmd) {
     // We have to check whether or not /bin/sh exists, but it should always exist so just return true
