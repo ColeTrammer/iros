a04c2fdef822bb8406f1b0257179667544cf2361
ColeTrammer <cole.trammer@gmail.com>
2022-08-07 23:14:49 +0000

    libliim: make IteratorContainer tuple like

--- libs/libliim/include/liim/container/producer/iterator_container.h
+++ libs/libliim/include/liim/container/producer/iterator_container.h
@@ -15,40 +15,45 @@ public:
     constexpr Iter begin() requires(!Copyable<Iter>) { return move(m_begin); }
     constexpr Iter end() requires(!Copyable<Iter>) { return move(m_end); }
 
+    template<size_t index>
+    constexpr Iter get() {
+        if constexpr (index == 0) {
+            if constexpr (Copyable<Iter>) {
+                return m_begin;
+            } else {
+                return move(m_begin);
+            }
+        } else if constexpr (index == 1) {
+            if constexpr (Copyable<Iter>) {
+                return m_end;
+            } else {
+                return move(m_end);
+            }
+        }
+    }
+
 private:
     Iter m_begin;
     Iter m_end;
 };
 
 template<Iterator Iter>
-class AutoSizedIteratorContainer {
+class AutoSizedIteratorContainer : public IteratorContainer<Iter> {
 public:
-    explicit constexpr AutoSizedIteratorContainer(Iter begin, Iter end) : m_begin(move(begin)), m_end(move(end)) {}
+    explicit constexpr AutoSizedIteratorContainer(Iter begin, Iter end) : IteratorContainer<Iter>(move(begin), move(end)) {}
 
-    constexpr Iter begin() const { return m_begin; }
-    constexpr Iter end() const { return m_end; }
-
-    constexpr auto size() const { return m_end - m_begin; }
-
-private:
-    Iter m_begin;
-    Iter m_end;
+    constexpr auto size() const { return this->end() - this->begin(); }
 };
 
 template<Iterator Iter>
-class ExplicitlySizedIteratorContainer {
+class ExplicitlySizedIteratorContainer : public IteratorContainer<Iter> {
 public:
     explicit constexpr ExplicitlySizedIteratorContainer(Iter begin, Iter end, size_t size)
-        : m_begin(move(begin)), m_end(move(end)), m_size(size) {}
-
-    constexpr Iter begin() const { return m_begin; }
-    constexpr Iter end() const { return m_end; }
+        : IteratorContainer<Iter>(move(begin), move(end)), m_size(size) {}
 
     constexpr size_t size() const { return m_size; }
 
 private:
-    Iter m_begin;
-    Iter m_end;
     size_t m_size;
 };
 
@@ -69,6 +74,38 @@ constexpr auto iterator_container(Iter begin, Iter end, size_t size) {
         return ExplicitlySizedIteratorContainer(move(begin), move(end), size);
     }
 }
+}
+
+namespace std {
+template<LIIM::Container::Iterator Iter>
+struct tuple_size<LIIM::Container::Producer::IteratorContainer<Iter>> {
+    constexpr static size_t value = 2;
+};
+
+template<size_t index, LIIM::Container::Iterator Iter>
+struct tuple_element<index, LIIM::Container::Producer::IteratorContainer<Iter>> {
+    using type = Iter;
+};
+
+template<LIIM::Container::Iterator Iter>
+struct tuple_size<LIIM::Container::Producer::AutoSizedIteratorContainer<Iter>> {
+    constexpr static size_t value = 2;
+};
+
+template<size_t index, LIIM::Container::Iterator Iter>
+struct tuple_element<index, LIIM::Container::Producer::AutoSizedIteratorContainer<Iter>> {
+    using type = Iter;
+};
+
+template<LIIM::Container::Iterator Iter>
+struct tuple_size<LIIM::Container::Producer::ExplicitlySizedIteratorContainer<Iter>> {
+    constexpr static size_t value = 2;
+};
+
+template<size_t index, LIIM::Container::Iterator Iter>
+struct tuple_element<index, LIIM::Container::Producer::ExplicitlySizedIteratorContainer<Iter>> {
+    using type = Iter;
+};
 }
 
 using LIIM::Container::Producer::iterator_container;
