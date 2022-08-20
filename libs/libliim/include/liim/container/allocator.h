24d5d38c20068babe07be3a12d879b00c3f6e6aa
ColeTrammer <cole.trammer@gmail.com>
2022-08-20 19:34:42 +0000

    libliim: make NewVector take an Allocator as a template parameter
    This patch makes NewVector's allocation strategy configurable, and
    also makes NewVector::data() and NewVector::span() constexpr.

---
+++ libs/libliim/include/liim/container/allocator.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <liim/container/allocator/allocator.h>
+#include <liim/container/allocator/standard_allocator.h>
+
+namespace LIIM::Container {
+using Allocators::Allocator;
+using Allocators::AllocatorOf;
+using Allocators::StandardAllocator;
+}
