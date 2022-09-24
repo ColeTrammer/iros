45990582baaec7288c500964b5f6137c36dc5d7a
ColeTrammer <cole.trammer@gmail.com>
2022-09-24 05:00:38 +0000

    iris: implement the x86_64 IDT and use it to handle seg faults

--- iris/boot/init.cpp
+++ iris/boot/init.cpp
@@ -1,4 +1,5 @@
 #include <di/prelude.h>
+#include <iris/arch/x86/amd64/idt.h>
 #include <iris/core/log.h>
 #include <limine.h>
 
@@ -11,9 +12,44 @@
     di::unreachable();
 }
 
+static void handler() {
+    iris::debug_log("Hello, World - from segfault\n"_sv);
+    done();
+}
+
+struct [[gnu::packed]] IDTR {
+    u16 size;
+    u64 virtual_address;
+};
+
+static inline void load_idt(IDTR descriptor) {
+    asm("lidtq %0" : : "m"(descriptor));
+}
+
+static auto idt = di::Array<iris::x86::amd64::idt::Entry, 256> {};
+
 extern "C" {
 void iris_main() {
     iris::debug_log("Hello, World\n"_sv);
+
+    using namespace iris::x86::amd64::idt;
+
+    auto handler_address = reinterpret_cast<u64>(&handler);
+    auto pf_entry = Entry(Present(true), Type(Type::InterruptGate), SegmentSelector(0x28), TargetLow(handler_address & 0xFFFF),
+                          TargetMid((handler_address >> 16) & 0xFFFF), TargetHigh(handler_address >> 32));
+    idt[14] = pf_entry;
+
+    auto idtr = IDTR { sizeof(idt) - 1, reinterpret_cast<u64>(idt.data()) };
+    load_idt(idtr);
+
+    iris::debug_log("Hello, World - again\n"_sv);
+
+    asm volatile("mov $0x1234, %rax\n"
+                 "xor %rax, %rax\n"
+                 "mov (%rax), %rax\n");
+
+    iris::debug_log("Hello, World - again...\n"_sv);
+
     done();
 }
 }
---
+++ iris/include/iris/arch/x86/amd64/idt.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <di/prelude.h>
+
+namespace iris::x86::amd64::idt {
+using namespace di::bit;
+
+// The format of the 64 bit Interrupt Descriptor Table is described
+// in the AMD64 Programmer's Manual; Volume 2; Figure 4-24, and on
+// the OSDEV wiki at the page:
+// https://wiki.osdev.org/Interrupt_Descriptor_Table#Structure_on_x86-64
+struct TargetLow : BitField<0, 16> {};
+struct SegmentSelector : BitField<16, 16> {};
+struct IST : BitField<32, 3> {};
+
+// Interrupt Gates automatically disable interrupts upon entry, while Trap Gates do not.
+// In practice, the system call vector should be made a Trap Gate, while hw IRQ handlers
+// and CPU exception handlers should be made Interrupt Gates.
+struct Type : BitField<40, 4> {
+    constexpr static u8 InterruptGate = 0b1110;
+    constexpr static u8 TrapGate = 0b1111;
+};
+
+struct DPL : BitField<45, 2> {};
+struct Present : BitFlag<47> {};
+struct TargetMid : BitField<48, 16> {};
+struct TargetHigh : BitField<64, 32> {};
+
+using Entry = di::bit::BitStruct<16, TargetLow, SegmentSelector, IST, Type, DPL, Present, TargetMid, TargetHigh>;
+}
