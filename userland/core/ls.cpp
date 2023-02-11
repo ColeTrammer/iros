a7759d12b88a9bdf0b578983099241f276028da8
ColeTrammer <cole.trammer@gmail.com>
2023-02-10 17:33:33 -0800

    ls: simple test program for dius::filesystem::DirectoryIterator

---
+++ userland/core/ls.cpp
@@ -0,0 +1,24 @@
+#include <dius/prelude.h>
+
+namespace ls {
+struct Args {
+    di::PathView path { "."_pv };
+
+    constexpr static auto get_cli_parser() {
+        return di::cli_parser<Args>("ls"_sv, "List directory contents"_sv)
+            .flag<&Args::path>('p', "path"_tsv, "Directory path to list from"_sv);
+    }
+};
+
+di::Result<void> main(Args& args) {
+    auto path = di::create<di::Path>(args.path);
+    auto iterator = TRY(di::create<dius::fs::DirectoryIterator>(di::move(path)));
+    for (auto directory : iterator) {
+        auto const& entry = TRY(directory);
+        dius::println("{}"_sv, entry.path());
+    }
+    return {};
+}
+}
+
+DIUS_MAIN(ls::Args, ls)
