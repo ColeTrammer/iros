8ff8e9d06a5ddea25826c4c479185d884ecf950c
ColeTrammer <cole.trammer@gmail.com>
2023-02-24 18:42:29 -0800

    di: export intrusive pointer type tags in prelude

--- libs/di/include/di/vocab/pointer/prelude.h
+++ libs/di/include/di/vocab/pointer/prelude.h
@@ -6,16 +6,20 @@
 #include <di/vocab/pointer/rc.h>
 
 namespace di {
+using vocab::AdoptObject;
 using vocab::Arc;
 using vocab::Box;
 using vocab::IntrusivePtr;
 using vocab::IntrusiveRefCount;
 using vocab::IntrusiveThreadUnsafeRefCount;
 using vocab::Rc;
+using vocab::RetainObject;
 
+using vocab::adopt_object;
 using vocab::make_arc;
 using vocab::make_box;
 using vocab::make_rc;
+using vocab::retain_object;
 using vocab::try_box;
 using vocab::try_make_arc;
 using vocab::try_make_rc;
