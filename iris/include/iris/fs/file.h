8df698d51bb3df09bda7c82263210c79e7f267c8
ColeTrammer <cole.trammer@gmail.com>
2023-02-26 17:43:36 -0800

    iris: implement files using di::AnyShared
    This allows the desired behavior of sharing files between tasks
    by default. In the future, files should not be shared by default,
    which effectively makes FD_CLOEXEC the default behavior. However,
    sharing files should still be possible (probably).

--- iris/include/iris/fs/file.h
+++ iris/include/iris/fs/file.h
@@ -20,31 +20,29 @@ struct WriteFileFunction
 
 constexpr inline auto write_file = WriteFileFunction {};
 
-using File = di::meta::List<WriteFileFunction>;
+using FileInterface = di::meta::List<WriteFileFunction>;
+using File = di::AnyShared<FileInterface>;
 
-class FileTable : public di::IntrusiveRefCount<FileTable> {
-private:
-    using FileStorage = di::AnyHybrid<File>;
-
+class FileTable {
 public:
-    Expected<FileStorage&> allocate_file_handle() {
-        for (auto& file : m_files) {
+    Expected<File&> allocate_file_handle() {
+        for (auto [i, file] : di::enumerate(m_files)) {
             if (file.empty()) {
                 // Set file to a dummy value so that it cannot be allocated
                 // again. The caller is expected to assign a proper value for
                 // this file descriptor.
-                file = di::Void {};
+                m_file_allocated[i] = true;
                 return file;
             }
         }
         return di::Unexpected(Error::TooManyFilesOpen);
     }
 
-    Expected<FileStorage&> lookup_file_handle(i32 file_handle) {
+    Expected<File&> lookup_file_handle(i32 file_handle) {
         if (file_handle < 0 || di::equal_or_greater(file_handle, m_files.size())) {
             return di::Unexpected(Error::BadFileDescriptor);
         }
-        if (m_files[file_handle].empty()) {
+        if (!m_file_allocated[file_handle] || m_files[file_handle].empty()) {
             return di::Unexpected(Error::BadFileDescriptor);
         }
         return m_files[file_handle];
@@ -54,15 +52,16 @@ public:
         if (file_handle < 0 || di::equal_or_greater(file_handle, m_files.size())) {
             return di::Unexpected(Error::BadFileDescriptor);
         }
-        if (m_files[file_handle].empty()) {
+        if (!m_file_allocated[file_handle] || m_files[file_handle].empty()) {
             return di::Unexpected(Error::BadFileDescriptor);
         }
         m_files[file_handle].reset();
+        m_file_allocated[file_handle] = false;
         return {};
     }
 
 private:
-    // FIXME: use di::Optional<FileStorage> once di::Any is optimized for it.
-    di::Array<FileStorage, 16> m_files {};
+    di::Array<File, 16> m_files {};
+    di::BitSet<16> m_file_allocated;
 };
 }
