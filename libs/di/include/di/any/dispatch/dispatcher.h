cc29deea5c6abe462aa3432847326df7116e8254
ColeTrammer <cole.trammer@gmail.com>
2023-03-16 04:50:31 +0000

    di: accept the exact arguments requested in di::Dispatcher

--- libs/di/include/di/any/dispatch/dispatcher.h
+++ libs/di/include/di/any/dispatch/dispatcher.h
@@ -23,7 +23,7 @@ namespace detail {
         template<typename T>
         requires(concepts::TagInvocableTo<Tag, R, meta::Like<Self, T>, Args...> ||
                  concepts::Disjunction<concepts::InvocableTo<Tags const&, R, meta::Like<Self, T>, Args...>...>)
-        constexpr R operator()(T&& self, Args&&... args) const {
+        constexpr R operator()(T&& self, Args... args) const {
             if constexpr (concepts::TagInvocableTo<Tag, R, meta::Like<Self, T>, Args...>) {
                 auto const tag = Tag {};
                 return function::tag_invoke(tag, util::forward_like<Self>(self), util::forward<Args>(args)...);
