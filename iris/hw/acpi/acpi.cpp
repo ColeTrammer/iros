f9d4ac8fd5d77ec8006be32c76c3250ce049eb08
ColeTrammer <cole.trammer@gmail.com>
2023-04-05 08:50:43 +0000

    iris: parse the MADT table
    This is the table essential for supporting SMP, as it contains
    information of the number of processors and the IO APIC configuration.

--- iris/hw/acpi/acpi.cpp
+++ iris/hw/acpi/acpi.cpp
@@ -39,7 +39,7 @@ void init_acpi() {
     println("RSDP signature: {}"_sv, signature);
     println("RSDP OEM ID: {}"_sv, oem_id);
     println("RSDP revision: {}"_sv, rsdp.revision);
-    println("RSDP RSDT address: {}"_sv, rsdp.rsdt_address);
+    println("RSDP RSDT address: {}"_sv, auto(rsdp.rsdt_address));
 
     if (!rsdp.validate_v1()) {
         println("RSDP has invalid checksum, panicking..."_sv);
@@ -65,10 +65,87 @@ void init_acpi() {
     auto rsdt_entries = rsdt.entries();
     println("RSDT entries: {}"_sv, rsdt_entries.size());
 
+    auto madt = di::Optional<MADT&> {};
     for (auto entry : rsdt_entries) {
         auto& header = mm::map_physical_address(mm::PhysicalAddress(entry), sizeof(SDTHeader))->typed<SDTHeader>();
         auto signature = byte_array_to_string_view(header.signature);
         println("Found ACPI table: {}"_sv, signature);
+
+        if (signature == "APIC"_tsv) {
+            if (!header.validate(sizeof(MADT))) {
+                println("MADT header is invalid, panicking..."_sv);
+                ASSERT(false);
+            }
+
+            madt = static_cast<MADT&>(header);
+        }
+    }
+
+    if (!madt) {
+        println("MADT not found, panicking..."_sv);
+        ASSERT(false);
+    }
+
+    for (auto interrupt_controller_structures = madt->interrupt_controller_structures();
+         interrupt_controller_structures != interrupt_controller_structures.end(); ++interrupt_controller_structures) {
+        auto const& interrupt_controller_structure = *interrupt_controller_structures;
+        switch (interrupt_controller_structure.type) {
+            case InterruptControllerStructureType::LocalApic: {
+                if (!interrupt_controller_structures.validate(sizeof(ProcessorLocalApicStructure))) {
+                    println("Local APIC structure is invalid, panicking..."_sv);
+                    ASSERT(false);
+                }
+
+                auto const& local_apic =
+                    static_cast<ProcessorLocalApicStructure const&>(interrupt_controller_structure);
+                println("Found local APIC: {}/{}"_sv, local_apic.processor_id, local_apic.apic_id);
+                break;
+            }
+            case InterruptControllerStructureType::IoApic: {
+                if (!interrupt_controller_structures.validate(sizeof(IoApicStructure))) {
+                    println("I/O APIC structure is invalid, panicking..."_sv);
+                    ASSERT(false);
+                }
+
+                auto const& io_apic = static_cast<IoApicStructure const&>(interrupt_controller_structure);
+                println("Found I/O APIC: {} => IRQ {}"_sv, io_apic.io_apic_id, io_apic.global_system_interrupt_base);
+                break;
+            }
+            case InterruptControllerStructureType::InterruptSourceOverride: {
+                if (!interrupt_controller_structures.validate(sizeof(InterruptSourceOverrideStructure))) {
+                    println("Interrupt source override structure is invalid, panicking..."_sv);
+                    ASSERT(false);
+                }
+
+                auto const& interrupt_source_override =
+                    static_cast<InterruptSourceOverrideStructure const&>(interrupt_controller_structure);
+                println("Found interrupt source override: Bus {}:{} => IRQ {} ({:#b})"_sv,
+                        interrupt_source_override.bus, interrupt_source_override.source,
+                        interrupt_source_override.global_system_interrupt, interrupt_source_override.flags.value);
+                break;
+            }
+            case InterruptControllerStructureType::LocalApicNmi: {
+                if (!interrupt_controller_structures.validate(sizeof(LocalApicNmiStructure))) {
+                    println("Local APIC NMI structure is invalid, panicking..."_sv);
+                    ASSERT(false);
+                }
+
+                auto const& local_apic_nmi = static_cast<LocalApicNmiStructure const&>(interrupt_controller_structure);
+                println("Found local APIC NMI: {} @ {} ({:#b})"_sv, local_apic_nmi.apic_processor_uid,
+                        local_apic_nmi.local_apic_lint, local_apic_nmi.flags.value);
+                break;
+            }
+            default: {
+                if (!interrupt_controller_structure.validate(sizeof(InterruptControllerStructureHeader))) {
+                    println("Unknown interrupt controller structure is invalid, panicking..."_sv);
+                    ASSERT(false);
+                }
+
+                println("Found unknown system interrupt controller of type: {}"_sv,
+                        di::to_underlying(interrupt_controller_structure.type));
+                break;
+            }
+        }
     }
 }
 }
--- iris/include/iris/hw/acpi/system_tables.h
+++ iris/include/iris/hw/acpi/system_tables.h
@@ -7,7 +7,7 @@ bool validate_acpi_checksum(di::Span<byte const> data);
 
 /// @brief Root System Description Pointer
 ///
-/// See https://wiki.osdev.org/RSDP or ACPI 6.5 spec section 5.2.5.3.
+/// See [OSDEV](https://wiki.osdev.org/RSDP) or ACPI 6.5 spec section 5.2.5.
 struct [[gnu::packed]] RSDP {
     /// @name Version 1.0 fields
     /// @{
@@ -32,7 +32,7 @@ struct [[gnu::packed]] RSDP {
 
 /// @brief System Description Table Header
 ///
-/// See https://wiki.osdev.org/RSDT#structure or ACPI 6.5 spec section 5.2.6.
+/// See [OSDEV](https://wiki.osdev.org/RSDT#structure) or ACPI 6.5 spec section 5.2.6.
 struct [[gnu::packed]] SDTHeader {
     di::Array<byte, 4> signature;
     u32 length;
@@ -56,11 +56,180 @@ struct [[gnu::packed]] SDTHeader {
 
 /// @brief Root System Description Table
 ///
-/// See https://wiki.osdev.org/RSDT or ACPI 6.5 spec section 5.2.7.
+/// See [OSDEV](https://wiki.osdev.org/RSDT) or ACPI 6.5 spec section 5.2.7.
 struct [[gnu::packed]] RSDT : SDTHeader {
-    /// @brief Array of pointers to other SDTs.
     di::Span<u32 const> entries() const {
         return di::Span { reinterpret_cast<u32 const*>(this + 1), (this->length - sizeof(SDTHeader)) / sizeof(u32) };
+    }
+};
+
+/// @brief Interrupt Controller Structure Type
+///
+/// See ACPI 6.5 spec section 5.2.12 table 5-21.
+enum class InterruptControllerStructureType : u8 {
+    LocalApic = 0x0,
+    IoApic = 0x1,
+    InterruptSourceOverride = 0x2,
+    NmiSource = 0x3,
+    LocalApicNmi = 0x4,
+    LocalApicAddressOverride = 0x5,
+    IoSapic = 0x6,
+    LocalSapic = 0x7,
+    PlatformInterruptSources = 0x8,
+    LocalX2Apic = 0x9,
+    LocalX2ApicNmi = 0xA,
+    GicCpuInterface = 0xB,
+    GicDistrutor = 0xC,
+    GicMsiFrame = 0xD,
+    GicRedistributor = 0xE,
+    GIGInterruptTranslationService = 0xF,
+    MultiprocessorWakeup = 0x10,
+    CorePic = 0x11,
+    LegacyPic = 0x12,
+    HyperTransportPic = 0x13,
+    ExtendIoPic = 0x14,
+    MsiPic = 0x15,
+    BridgeIoPic = 0x16,
+    LowPinCountPic = 0x17,
+};
+
+/// @brief Interrupt Controller Structure Header
+///
+/// See ACPI 6.5 spec section 5.2.12.
+struct InterruptControllerStructureHeader {
+    InterruptControllerStructureType type;
+    u8 length;
+
+    InterruptControllerStructureHeader const* next() const {
+        return reinterpret_cast<InterruptControllerStructureHeader const*>(reinterpret_cast<byte const*>(this) +
+                                                                           this->length);
+    }
+
+    bool validate(usize min_length) const { return this->length >= min_length; }
+};
+
+/// @brief Processor Local APIC Structure
+///
+/// See [OSDEV](https://wiki.osdev.org/MADT#Entry_Type_0:_Processor_Local_APIC) or ACPI 6.5 spec section 5.2.12.2.
+struct [[gnu::packed]] ProcessorLocalApicStructure : InterruptControllerStructureHeader {
+    enum class Flags : u32 {
+        Enabled = 1,
+        OnlineCapable = 2,
+    };
+
+    u8 processor_id;
+    u8 apic_id;
+    Flags flags;
+};
+
+/// @brief I/O APIC Structure
+///
+/// See [OSDEV](https://wiki.osdev.org/MADT#Entry_Type_1:_I.2FO_APIC) or ACPI 6.5 spec section 5.2.12.3.
+struct [[gnu::packed]] IoApicStructure : InterruptControllerStructureHeader {
+    u8 io_apic_id;
+    u8 reserved;
+    u32 io_apic_address;
+    u32 global_system_interrupt_base;
+};
+
+/// @brief MPS Interrupt Flags
+///
+/// @note These flags apply to several structures, and are originally defined in the MPS 1.4 spec, but now referenced by
+/// the ACPI 6.5 spec.
+///
+/// See [OSDEV](https://wiki.osdev.org/MADT#Flags) or ACPI 6.5 spec section 5.2.12.5 table 5-26.
+struct [[gnu::packed]] MPSInterruptFlags {
+    enum class Polarity {
+        BusDefined = 0,
+        ActiveHigh = 1,
+        Reserved = 2,
+        ActiveLow = 3,
+    };
+
+    enum class TriggerMode {
+        BusDefined = 0,
+        Edge = 1,
+        Reserved = 2,
+        Level = 3,
+    };
+
+    Polarity polarity() const { return Polarity(value & 0b11); }
+    TriggerMode trigger_mode() const { return TriggerMode((value & 0b1100) >> 2); }
+
+    u16 value;
+};
+
+/// @brief Interrupt Source Override Structure
+///
+/// See [OSDEV](https://wiki.osdev.org/MADT#Entry_Type_2:_IO.2FAPIC_Interrupt_Source_Override) or ACPI 6.5 spec
+/// section 5.2.12.5.
+struct [[gnu::packed]] InterruptSourceOverrideStructure : InterruptControllerStructureHeader {
+    u8 bus;
+    u8 source;
+    u32 global_system_interrupt;
+    MPSInterruptFlags flags;
+};
+
+/// @brief Local APIC NMI Structure
+///
+/// See [OSDEV](https://wiki.osdev.org/MADT#Entry_Type_4:_Local_APIC_Non-maskable_interrupts) or ACPI 6.5 spec
+/// section 5.2.12.17.
+struct [[gnu::packed]] LocalApicNmiStructure : InterruptControllerStructureHeader {
+    u8 apic_processor_uid;
+    MPSInterruptFlags flags;
+    u8 local_apic_lint;
+};
+
+class InterruptControllerStructureIterator
+    : public di::container::IteratorBase<InterruptControllerStructureIterator, di::ForwardIteratorTag,
+                                         InterruptControllerStructureHeader, isize> {
+public:
+    InterruptControllerStructureIterator() = default;
+
+    explicit InterruptControllerStructureIterator(InterruptControllerStructureHeader const* current,
+                                                  InterruptControllerStructureHeader const* end)
+        : m_current(current), m_end(end) {}
+
+    InterruptControllerStructureHeader const& operator*() const { return *m_current; }
+    InterruptControllerStructureHeader const* operator->() const { return m_current; }
+
+    bool validate(usize min_size) const {
+        auto const* readable_end = reinterpret_cast<byte const*>(m_current) + min_size;
+        return m_current->validate(min_size) && m_current->next() <= m_end &&
+               di::to_uintptr(readable_end) <= di::to_uintptr(m_end);
+    }
+
+    void advance_one() { m_current = m_current->next(); }
+
+    InterruptControllerStructureIterator begin() const { return *this; }
+    InterruptControllerStructureIterator end() const { return InterruptControllerStructureIterator { m_end, m_end }; }
+
+private:
+    friend bool operator==(InterruptControllerStructureIterator const& a,
+                           InterruptControllerStructureIterator const& b) {
+        return a.m_current == b.m_current;
+    }
+
+    InterruptControllerStructureHeader const* m_current { nullptr };
+    InterruptControllerStructureHeader const* m_end { nullptr };
+};
+
+/// @brief Multiple APIC Description Table
+///
+/// See [OSDEV](https://wiki.osdev.org/MADT) or ACPI 6.5 spec section 5.2.12.
+struct [[gnu::packed]] MADT : SDTHeader {
+    enum class Flags : u32 {
+        PcAtCompatible = 1,
+    };
+
+    u32 local_apic_address;
+    Flags flags;
+
+    InterruptControllerStructureIterator interrupt_controller_structures() const {
+        return InterruptControllerStructureIterator {
+            reinterpret_cast<InterruptControllerStructureHeader const*>(this + 1),
+            reinterpret_cast<InterruptControllerStructureHeader const*>(this->as_bytes().end())
+        };
     }
 };
 
