438f8bc6f3a184fdcee626e104b5518a56aaaa23
ColeTrammer <cole.trammer@gmail.com>
2023-04-15 06:58:59 +0000

    ccpp: use Checked<T> for calloc

--- libs/ccpp/stdlib/calloc.cpp
+++ libs/ccpp/stdlib/calloc.cpp
@@ -1,14 +1,20 @@
+#include <di/math/intcmp/prelude.h>
+#include <di/prelude.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 
 extern "C" void* calloc(size_t count, size_t size) {
-    // FIXME: check for overflow.
-    auto true_size = count * size;
+    auto true_size = di::Checked(count) * size;
+    if (!true_size.valid()) {
+        errno = EOVERFLOW;
+        return nullptr;
+    }
 
-    auto* result = malloc(true_size);
+    auto* result = malloc(*true_size.value());
     if (!result) {
         return nullptr;
     }
-    memset(result, 0, true_size);
+    memset(result, 0, *true_size.value());
     return result;
 }
