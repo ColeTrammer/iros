d14ba8c228f74830d9233b12e448cd2cbd989f81
ColeTrammer <cole.trammer@gmail.com>
2023-04-27 00:15:38 -0700

    dius: export gcc int functions as weak
    This prevents conflicts with libgcc, which also exports these functions.

--- libs/dius/runtime/gccints.cpp
+++ libs/dius/runtime/gccints.cpp
@@ -4,12 +4,12 @@
 __extension__ typedef int raw_i128 __attribute__((mode(TI)));
 __extension__ typedef unsigned raw_u128 __attribute__((mode(TI)));
 
-extern "C" raw_u128 __udivti3(raw_u128 a, raw_u128 b) {
+extern "C" [[gnu::weak]] raw_u128 __udivti3(raw_u128 a, raw_u128 b) {
     return di::bit_cast<raw_u128>(di::bit_cast<di::u128_fallback>(a) / di::bit_cast<di::u128_fallback>(b));
 }
-extern "C" raw_u128 __umodti3(raw_u128 a, raw_u128 b) {
+extern "C" [[gnu::weak]] raw_u128 __umodti3(raw_u128 a, raw_u128 b) {
     return di::bit_cast<raw_u128>(di::bit_cast<di::u128_fallback>(a) % di::bit_cast<di::u128_fallback>(b));
 }
-extern "C" raw_i128 __divti3(raw_i128 a, raw_i128 b) {
+extern "C" [[gnu::weak]] raw_i128 __divti3(raw_i128 a, raw_i128 b) {
     return di::bit_cast<raw_i128>(di::bit_cast<di::i128_fallback>(a) / di::bit_cast<di::i128_fallback>(b));
 }
