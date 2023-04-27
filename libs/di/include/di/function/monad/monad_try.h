f786e61f88e42131eca910b548dd1a7d24b97384
ColeTrammer <cole.trammer@gmail.com>
2023-04-27 05:07:30 +0000

    di: disable -Wdangling-reference in GCC 13

--- libs/di/include/di/function/monad/monad_try.h
+++ libs/di/include/di/function/monad/monad_try.h
@@ -2,6 +2,14 @@
 
 #include <di/util/move.h>
 
+#if __GNUC__ >= 13
+// NOTE: GCC 13 thinks that calling DI_TRY() on a di::Result<int&> produces a dangling reference, because the result
+// object gets destroyed before the reference is used. However, di::Result<int&> is essentially a pointer type, so this
+// is not a problem. Unfortunately, since this occurs in a macro, this warning cannot be suppressed only inside this
+// header file.
+#pragma GCC diagnostic ignored "-Wdangling-reference"
+#endif
+
 #define DI_TRY(...)                                           \
     __extension__({                                           \
         auto __result = (__VA_ARGS__);                        \
