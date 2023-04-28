9966e63f1bf2d1c5569774bf6beea80c0af1b1b7
ColeTrammer <cole.trammer@gmail.com>
2023-04-28 08:53:43 +0000

    dius: make cxxabi symbols weak
    This allows libsupc++ to override these symbols.

--- libs/dius/runtime/cxxabi.cpp
+++ libs/dius/runtime/cxxabi.cpp
@@ -3,7 +3,7 @@
 #include <di/util/prelude.h>
 
 namespace __cxxabiv1 {
-extern "C" [[noreturn]] void __cxa_pure_virtual() {
+extern "C" [[gnu::weak]] [[noreturn]] void __cxa_pure_virtual() {
     ASSERT(false);
     di::unreachable();
 }
@@ -14,7 +14,7 @@ extern "C" int __cxa_atexit(void (*)(void*), void*, void*) {
 
 __extension__ typedef int __guard __attribute__((mode(__DI__)));
 
-extern "C" int __cxa_guard_acquire(__guard* guard) {
+extern "C" [[gnu::weak]] int __cxa_guard_acquire(__guard* guard) {
     __guard expected = 0;
     if (di::AtomicRef(*guard).compare_exchange_strong(expected, 1, di::MemoryOrder::AcquireRelease)) {
         return 1;
@@ -27,7 +27,7 @@ extern "C" int __cxa_guard_acquire(__guard* guard) {
     return 0;
 }
 
-extern "C" void __cxa_guard_release(__guard* guard) {
+extern "C" [[gnu::weak]] void __cxa_guard_release(__guard* guard) {
     di::AtomicRef(*guard).store(2, di::MemoryOrder::Release);
 }
 }
