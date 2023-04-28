a6b3de8f05217eb8ecedcdf91c651b868ae097ee
ColeTrammer <cole.trammer@gmail.com>
2023-04-28 14:19:05 -0700

    dius: mark cxx heap functions as weak

--- libs/dius/runtime/cxxheap.cpp
+++ libs/dius/runtime/cxxheap.cpp
@@ -2,57 +2,58 @@
 #include <dius/print.h>
 #include <dius/system/prelude.h>
 
-void* operator new(std::size_t size) {
+[[gnu::weak]] void* operator new(std::size_t size) {
     return ::operator new(size, std::align_val_t { alignof(void*) });
 }
-void* operator new(std::size_t size, std::align_val_t alignment) {
+[[gnu::weak]] void* operator new(std::size_t size, std::align_val_t alignment) {
     auto* result = ::operator new(size, alignment, std::nothrow);
     ASSERT(result);
     return result;
 }
 
-void* operator new(std::size_t size, std::nothrow_t const&) noexcept {
+[[gnu::weak]] void* operator new(std::size_t size, std::nothrow_t const&) noexcept {
     return ::operator new(size, std::align_val_t { alignof(void*) }, std::nothrow);
 }
 
-void operator delete(void* pointer, std::nothrow_t const&) noexcept {
+[[gnu::weak]] void operator delete(void* pointer, std::nothrow_t const&) noexcept {
     return ::operator delete(pointer);
 }
-void operator delete(void* pointer, std::align_val_t alignment, std::nothrow_t const&) noexcept {
+[[gnu::weak]] void operator delete(void* pointer, std::align_val_t alignment, std::nothrow_t const&) noexcept {
     return ::operator delete(pointer, alignment);
 }
 
 // Array allocating new.
-[[nodiscard]] void* operator new[](std::size_t size) {
+[[nodiscard]] [[gnu::weak]] void* operator new[](std::size_t size) {
     return ::operator new(size);
 }
-[[nodiscard]] void* operator new[](std::size_t size, std::align_val_t alignment) {
+[[nodiscard]] [[gnu::weak]] void* operator new[](std::size_t size, std::align_val_t alignment) {
     return ::operator new(size, alignment);
 }
-[[nodiscard]] void* operator new[](std::size_t size, std::nothrow_t const&) noexcept {
+[[nodiscard]] [[gnu::weak]] void* operator new[](std::size_t size, std::nothrow_t const&) noexcept {
     return ::operator new(size, std::nothrow);
 }
-[[nodiscard]] void* operator new[](std::size_t size, std::align_val_t alignment, std::nothrow_t const&) noexcept {
+[[nodiscard]] [[gnu::weak]] void* operator new[](std::size_t size, std::align_val_t alignment,
+                                                 std::nothrow_t const&) noexcept {
     return ::operator new(size, alignment, std::nothrow);
 }
 
 // Array deallocating delete.
-void operator delete[](void* pointer) noexcept {
+[[gnu::weak]] void operator delete[](void* pointer) noexcept {
     return ::operator delete(pointer);
 }
-void operator delete[](void* pointer, std::size_t size) noexcept {
+[[gnu::weak]] void operator delete[](void* pointer, std::size_t size) noexcept {
     return ::operator delete(pointer, size);
 }
-void operator delete[](void* pointer, std::align_val_t alignment) noexcept {
+[[gnu::weak]] void operator delete[](void* pointer, std::align_val_t alignment) noexcept {
     return ::operator delete(pointer, alignment);
 }
-void operator delete[](void* pointer, std::size_t size, std::align_val_t alignment) noexcept {
+[[gnu::weak]] void operator delete[](void* pointer, std::size_t size, std::align_val_t alignment) noexcept {
     return ::operator delete(pointer, size, alignment);
 }
-void operator delete[](void* pointer, std::nothrow_t const&) noexcept {
+[[gnu::weak]] void operator delete[](void* pointer, std::nothrow_t const&) noexcept {
     return ::operator delete(pointer, std::nothrow);
 }
-void operator delete[](void* pointer, std::align_val_t alignment, std::nothrow_t const&) noexcept {
+[[gnu::weak]] void operator delete[](void* pointer, std::align_val_t alignment, std::nothrow_t const&) noexcept {
     return ::operator delete(pointer, alignment, std::nothrow);
 }
 
@@ -60,7 +61,7 @@ void operator delete[](void* pointer, std::align_val_t alignment, std::nothrow_t
 static uptr heap_end;
 #endif
 
-void* operator new(std::size_t size, std::align_val_t align, std::nothrow_t const&) noexcept {
+[[gnu::weak]] void* operator new(std::size_t size, std::align_val_t align, std::nothrow_t const&) noexcept {
 #ifdef DIUS_PLATFORM_LINUX
     if (!heap_end) {
         heap_end = *dius::system::system_call<uptr>(dius::system::Number::brk, nullptr);
@@ -90,7 +91,7 @@ void* operator new(std::size_t size, std::align_val_t align, std::nothrow_t cons
 }
 
 // Deallocating delete.
-void operator delete(void*) noexcept {}
-void operator delete(void*, std::size_t) noexcept {}
-void operator delete(void*, std::align_val_t) noexcept {}
-void operator delete(void*, std::size_t, std::align_val_t) noexcept {}
+[[gnu::weak]] void operator delete(void*) noexcept {}
+[[gnu::weak]] void operator delete(void*, std::size_t) noexcept {}
+[[gnu::weak]] void operator delete(void*, std::align_val_t) noexcept {}
+[[gnu::weak]] void operator delete(void*, std::size_t, std::align_val_t) noexcept {}
