259ce2793e451275c13b65134c359696ac9b4f1c
ColeTrammer <cole.trammer@gmail.com>
2024-06-15 14:22:38 -0700

    ccpp: add minimal stddef.h header

---
+++ libs/ccpp/include/stddef.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <ccpp/bits/config.h>
+#include <ccpp/bits/null.h>
+#include <ccpp/bits/size_t.h>
+
+__CCPP_BEGIN_DECLARATIONS
+
+#define offsetof __builtin_offsetof
+
+__CCPP_END_DECLARATIONS
