c771d5dbfd0a01ebe10ed54486f223ce2bcb3cce
ColeTrammer <cole.trammer@gmail.com>
2024-06-15 14:33:13 -0700

    meta: switch nix autoformatter to rfc style

--- flake.nix
+++ flake.nix
@@ -8,111 +8,117 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.treefmt-nix.flakeModule
         inputs.flake-root.flakeModule
       ];
-      systems = ["x86_64-linux"];
-      perSystem = {
-        config,
-        pkgs,
-        system,
-        ...
-      }: let
-        gccVersion = "14";
-        llvmVersion = "18";
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          gccVersion = "14";
+          llvmVersion = "18";
 
-        clang = pkgs."llvmPackages_${llvmVersion}".libcxxClang;
-        clangTools = pkgs."clang-tools_${llvmVersion}".override {
-          enableLibcxx = true;
-        };
+          clang = pkgs."llvmPackages_${llvmVersion}".libcxxClang;
+          clangTools = pkgs."clang-tools_${llvmVersion}".override { enableLibcxx = true; };
 
-        lldb = pkgs."lldb_${llvmVersion}";
+          lldb = pkgs."lldb_${llvmVersion}";
 
-        stdenv = pkgs."gcc${gccVersion}Stdenv";
-      in {
-        treefmt = {
-          inherit (config.flake-root) projectRootFile;
+          stdenv = pkgs."gcc${gccVersion}Stdenv";
+        in
+        {
+          treefmt = {
+            inherit (config.flake-root) projectRootFile;
 
-          programs = {
-            alejandra.enable = true;
-            clang-format = {
-              enable = true;
-              package = clangTools;
+            programs = {
+              clang-format = {
+                enable = true;
+                package = clangTools;
+              };
+              nixfmt-rfc-style.enable = true;
+              prettier.enable = true;
+              shfmt = {
+                enable = true;
+                indent_size = 4;
+              };
+              just.enable = true;
             };
-            prettier.enable = true;
-            shfmt = {
-              enable = true;
-              indent_size = 4;
+
+            settings.formatter.cmake-format = {
+              command = "${pkgs.cmake-format}/bin/cmake-format";
+              options = [ "-i" ];
+              includes = [
+                "CMakeLists.txt"
+                "CMakeToolchain.*.txt"
+                "*.cmake"
+              ];
             };
-            just.enable = true;
           };
 
-          settings.formatter.cmake-format = {
-            command = "${pkgs.cmake-format}/bin/cmake-format";
-            options = ["-i"];
-            includes = ["CMakeLists.txt" "CMakeToolchain.*.txt" "*.cmake"];
+          devShells.default = pkgs.mkShell.override { inherit stdenv; } {
+            packages =
+              [
+                config.treefmt.build.wrapper
+                pkgs.cmake-format
+              ]
+              ++ builtins.attrValues config.treefmt.build.programs
+              ++ [
+                # Clang
+                clangTools
+                clang
+
+                # Debug
+                pkgs.gdb
+                pkgs.valgrind
+                lldb
+
+                # justfile build commands
+                pkgs.fzf
+                pkgs.jq
+                pkgs.just
+
+                # Linux build
+                pkgs.cmake
+                pkgs.ninja
+                pkgs.ccache
+
+                # Docs
+                pkgs.doxygen
+                pkgs.graphviz
+
+                # Cross compiler deps
+                pkgs.bison
+                pkgs.flex
+                pkgs.mpfr
+                pkgs.gmp
+                pkgs.libmpc
+                pkgs.autoconf269
+                pkgs.automake115x
+
+                # Build and run Iros images
+                pkgs.qemu
+                pkgs.parted
+
+                # Coverage
+                pkgs.gcovr
+
+                # Linux deps
+                pkgs.pipewire
+                pkgs.wayland-scanner
+                pkgs.wayland
+              ];
+
+            # Needed to build the cross compilr
+            hardeningDisable = [ "format" ];
           };
         };
-
-        devShells.default = pkgs.mkShell.override {inherit stdenv;} {
-          packages =
-            [
-              config.treefmt.build.wrapper
-              pkgs.cmake-format
-            ]
-            ++ builtins.attrValues config.treefmt.build.programs
-            ++ [
-              # Clang
-              clangTools
-              clang
-
-              # Debug
-              pkgs.gdb
-              pkgs.valgrind
-              lldb
-
-              # justfile build commands
-              pkgs.fzf
-              pkgs.jq
-              pkgs.just
-
-              # Linux build
-              pkgs.cmake
-              pkgs.ninja
-              pkgs.ccache
-
-              # Docs
-              pkgs.doxygen
-              pkgs.graphviz
-
-              # Cross compiler deps
-              pkgs.bison
-              pkgs.flex
-              pkgs.mpfr
-              pkgs.gmp
-              pkgs.libmpc
-              pkgs.autoconf269
-              pkgs.automake115x
-
-              # Build and run Iros images
-              pkgs.qemu
-              pkgs.parted
-
-              # Coverage
-              pkgs.gcovr
-
-              # Linux deps
-              pkgs.pipewire
-              pkgs.wayland-scanner
-              pkgs.wayland
-            ];
-
-          # Needed to build the cross compilr
-          hardeningDisable = ["format"];
-        };
-      };
     };
 }
