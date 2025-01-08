61f2b045ef6363d42de95c02b41d17f1f937654e
ColeTrammer <cole.trammer@gmail.com>
2025-01-08 00:09:36 -0800

    dius: add file actions to process spawn API

--- libs/dius/include/dius/system/process.h
+++ libs/dius/include/dius/system/process.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "di/assert/prelude.h"
+#include "di/container/path/path.h"
 #include "di/container/string/prelude.h"
 #include "di/container/vector/prelude.h"
 #include "di/util/prelude.h"
 #include "dius/config.h"
 #include "dius/error.h"
+#include "dius/sync_file.h"
 
 #include DIUS_PLATFORM_PATH(process.h)
 
@@ -34,13 +36,50 @@ private:
 };
 
 class Process {
+    struct FileAction {
+        enum class Type {
+            Open,
+            Dup,
+            Close,
+        };
+
+        Type type { Type::Dup };
+        i32 arg0 = 0;
+        i32 arg1 = 0;
+        i32 arg2 = 0;
+        di::Path path;
+    };
+
 public:
     explicit Process(di::Vector<di::TransparentString> arguments) : m_arguments(di::move(arguments)) {}
+
+    auto with_file_open(i32 fd, di::Path path, OpenMode open_mode, u16 create_mode = 0666) && -> Process {
+        m_file_actions.push_back(
+            { FileAction::Type::Open, fd, static_cast<i32>(open_mode), i32(create_mode), di::move(path) });
+        return di::move(*this);
+    }
+
+    auto with_file_close(i32 fd) && -> Process {
+        m_file_actions.push_back({ FileAction::Type::Close, fd, 0, 0, {} });
+        return di::move(*this);
+    }
+
+    auto with_file_dup(i32 old_fd, i32 new_fd) && -> Process {
+        m_file_actions.push_back({ FileAction::Type::Dup, old_fd, new_fd, 0, {} });
+        return di::move(*this);
+    }
+
+    auto with_new_session() && -> Process {
+        m_new_session = true;
+        return di::move(*this);
+    }
 
     auto spawn_and_wait() && -> di::Result<ProcessResult>;
 
 private:
     di::Vector<di::TransparentString> m_arguments;
+    di::Vector<FileAction> m_file_actions;
+    bool m_new_session { false };
 };
 
 /// @brief Exit the currently executing thread.
--- libs/dius/posix/system/process.cpp
+++ libs/dius/posix/system/process.cpp
@@ -7,14 +7,67 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include "di/util/scope_exit.h"
+
 namespace dius::system {
+static auto file_open_mode_flags(OpenMode open_mode) -> int {
+    switch (open_mode) {
+        case OpenMode::Readonly:
+            return O_RDONLY;
+        case OpenMode::WriteNew:
+            return O_WRONLY | O_EXCL | O_CREAT;
+        case OpenMode::WriteClobber:
+            return O_WRONLY | O_TRUNC | O_CREAT;
+        case OpenMode::ReadWrite:
+            return O_RDWR;
+        case OpenMode::AppendOnly:
+            return O_WRONLY | O_APPEND | O_CREAT;
+        case OpenMode::ReadWriteClobber:
+            return O_RDWR | O_TRUNC | O_CREAT;
+        case OpenMode::AppendReadWrite:
+            return O_RDWR | O_APPEND | O_CREAT;
+        default:
+            di::unreachable();
+    }
+}
+
 auto Process::spawn_and_wait() && -> di::Result<ProcessResult> {
     // NOTE: TransparentString objects are guaranteed to be null-terminated on Linux.
     auto null_terminated_args =
         di::concat(m_arguments | di::transform(di::cdata), di::single(nullptr)) | di::to<di::Vector>();
 
+    auto file_actions = posix_spawn_file_actions_t {};
+    posix_spawn_file_actions_init(&file_actions);
+    auto file_action_cleanup = di::ScopeExit([&] {
+        posix_spawn_file_actions_destroy(&file_actions);
+    });
+    for (auto const& action : m_file_actions) {
+        switch (action.type) {
+            case FileAction::Type::Open:
+                posix_spawn_file_actions_addopen(&file_actions, action.arg0, action.path.c_str(),
+                                                 file_open_mode_flags(static_cast<OpenMode>(action.arg1)), action.arg2);
+                break;
+            case FileAction::Type::Close:
+                posix_spawn_file_actions_addclose(&file_actions, action.arg0);
+                break;
+            case FileAction::Type::Dup:
+                posix_spawn_file_actions_adddup2(&file_actions, action.arg0, action.arg1);
+                break;
+            default:
+                return di::Unexpected(di::BasicError::InvalidArgument);
+        }
+    }
+
+    auto attrs = posix_spawnattr_t {};
+    posix_spawnattr_init(&attrs);
+    auto attrs_guard = di::ScopeExit([&] {
+        posix_spawnattr_destroy(&attrs);
+    });
+    auto attr_flags = m_new_session ? POSIX_SPAWN_SETSID : 0;
+    posix_spawnattr_setflags(&attrs, attr_flags);
+
     auto pid = pid_t(-1);
-    auto result = posix_spawnp(&pid, null_terminated_args[0], nullptr, nullptr,
+    auto result = posix_spawnp(&pid, null_terminated_args[0], &file_actions, &attrs,
                                const_cast<char**>(null_terminated_args.data()), environ);
     if (result != 0) {
         return di::Unexpected(di::BasicError(result));
